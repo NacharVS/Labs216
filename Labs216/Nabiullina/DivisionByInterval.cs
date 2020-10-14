b696d7c8cccf0bd783bf8ec57a214f28031a47a5
Zulfia <zulfianabiullina4998@.com>
2020-10-14 23:31:02 +0300

    cdelala nomer 2

--- Labs216/Nabiullina/DivisionByInterval.cs
+++ Labs216/Nabiullina/DivisionByInterval.cs
@@ -6,8 +6,11 @@ namespace Labs216.Nabiullina
 {
     class DivisionByInterval
     {
-       
-        static public void DivisionbyInterval()
+       static void Main(string[] args)
+        {
+            DivisionByInterval2();
+        }
+        static public void DivisionByInterval1()
         {
             Console.WriteLine("Enter a namber:");
             double a = double.Parse (Console.ReadLine());
@@ -25,6 +28,20 @@ namespace Labs216.Nabiullina
             }
         }
         
+        static public void DivisionByInterval2()
+        {
+            Console.WriteLine(" The solution to ax^2-bx+c");
+            Console.Write("a = ");
+            int a = int.Parse (Console.ReadLine());
+            Console.Write("x = ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("c = ");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine($" Answer: {a}*{x}^2-{b}*{x}+{c}={a * Math.Pow(x, 2) - b * x + c}");
+        }
+
     }
 
 }
