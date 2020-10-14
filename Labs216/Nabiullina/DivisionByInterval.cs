4b68bcb3eaa0c76e4b68d676da9953c44c0a76e4
Zulfia <zulfianabiullina4998@.com>
2020-10-14 19:38:57 +0300

    cdelala doma

--- Labs216/Nabiullina/DivisionByInterval.cs
+++ Labs216/Nabiullina/DivisionByInterval.cs
@@ -6,9 +6,25 @@ namespace Labs216.Nabiullina
 {
     class DivisionByInterval
     {
-        static public void DivisionByInterval()
+       
+        static public void DivisionbyInterval()
         {
+            Console.WriteLine("Enter a namber:");
+            double a = double.Parse (Console.ReadLine());
 
+            Console.WriteLine("Division from what namber?");
+            double x = double.Parse (Console.ReadLine());
+
+            Console.WriteLine("Division from what namber?");
+            double y = double.Parse (Console.ReadLine());
+            for (double b = x; b <= y; b++ )
+            {
+                double z = a / b;
+                Console.WriteLine($"{a} divided by {b} = {z}");
+
+            }
         }
+        
     }
+
 }
