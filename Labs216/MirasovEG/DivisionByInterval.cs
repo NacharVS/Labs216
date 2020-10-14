ea1ef3c6333d9cd1ae241c39ad0af3ddf4a73fd1
Mir-ax <mirasov.egor@mail.ru>
2020-10-14 14:30:45 +0300

    Division by interval

---
+++ Labs216/MirasovEG/DivisionByInterval.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.MirasovEG
+{
+    class DivisionByInterval
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter begin interval");
+            int first = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter end interval");
+            int second = int.Parse(Console.ReadLine());
+
+            for (int i = first; i <= second; i++)
+            {
+                double devision = number / i;
+                Console.WriteLine($"Результат деления {number} на {i} = {devision}");
+            }
+        }
+        
+    }
+}
