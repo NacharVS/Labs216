0359f8e9123fcd85f90579c20c80b54e90e50f55
Zulfia <zulfianabiullina4998@.com>
2020-10-14 15:08:48 +0300

    poshla domoi

---
+++ Labs216/Nabiullina/DivisionByInterval.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Nabiullina
+{
+    class DivisionByInterval
+    {
+        static public void DivisionByInterval()
+        {
+
+        }
+    }
+}
