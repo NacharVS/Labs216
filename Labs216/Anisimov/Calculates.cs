9d9b535fd894f87cd28b5a5aba60821d3d17fffa
AnisimovIvan <anisimovivan9903@gmail.com>
2020-10-15 09:15:17 -0600

    Calculates and DivisionByInterval

--- Labs216/Anisimov/Calculates.cs
+++ Labs216/Anisimov/Calculates.cs
@@ -4,99 +4,136 @@ using System.Text;
 
 namespace Labs216.Anisimov
 {
-    class Calculates
-    {
-        //Use !Program.cs to run 
-        public static void QuadraticEquation()
+        class Calculates
         {
-            Console.WriteLine("Enter a:");
-            double a = double.Parse(Console.ReadLine());
-            Console.WriteLine("Enter b:");
-            double b = double.Parse(Console.ReadLine());
-            Console.WriteLine("Enter c:");
-            double c = double.Parse(Console.ReadLine());
-            double d = b * b - 4 * a * c;
-            double sqrt_d = Math.Sqrt(d);
-            double x1 = (-b + sqrt_d) / (2 * a);
-            double x2 = (-b - sqrt_d) / (2 * a);
-            if (d == 0)
-                Console.WriteLine($"X = {x1}");
-            else
-                Console.WriteLine($"X = {x1} ; {x2}");
-        }
-        public static void PythagoreanTheorem()
-        {
-            Console.WriteLine("Type -1 in the value you want to find (It will find c and c^2 by defualt)");
-            Console.Write("a:");
-            double a = double.Parse(Console.ReadLine());
-            Console.Write("b:");
-            double b = double.Parse(Console.ReadLine());
-            Console.Write("c:");
-            double c = double.Parse(Console.ReadLine());
-            if (a == -1) //Пользователь может выберать что ему надо, в зависимости от того какие данные у него есть
+            /*
+            static void Main(string[] args)
             {
-                a = c * c - b * b;
-                Console.WriteLine($"hypotenuse square -- {a} | hypotenuse -- {Math.Sqrt(a)}");
+                string chk = "y";
+                while (chk == "y")
+                {
+                    Console.WriteLine("What do you want to calculate? 1 -- ax^2 - bx + c || 2 -- c^2=a^2+b^2 || 3 -- circumference || 4 -- area of circle || 5 -- parallelepiped's Volume || 6 -- amperage|resistanese|voltage.");
+                    int choose = int.Parse(Console.ReadLine());
+                    switch (choose)
+                    {
+                        case 1:
+                            Calculates.QuadraticEquation();
+                            break;
+                        case 2:
+                            Calculates.PythagoreanTheorem();
+                            break;
+                        case 3:
+                            Calculates.Circumference();
+                            break;
+                        case 4:
+                            Calculates.AreaOfCircle();
+                            break;
+                        case 5:
+                            Calculates.ParallepipedVolume();
+                            break;
+                        case 6:
+                            Calculates.AmperResVolt();
+                            break;
+                        default:
+                            Console.WriteLine($"Wrong input. ({choose})");
+                            break;
+                    }
+                    Console.WriteLine("Continue? y/n");
+                    chk = Console.ReadLine();
+                }
             }
-            else if (b == -1)
+            */
+            public static void QuadraticEquation()
             {
-                b = c * c - a * a;
-                Console.WriteLine($"hypotenuse square -- {b} | hypotenuse -- {Math.Sqrt(b)}");
+                Console.WriteLine("Enter a:");
+                double a = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter b:");
+                double b = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter c:");
+                double c = double.Parse(Console.ReadLine());
+                double d = b * b - 4 * a * c;
+                double sqrt_d = Math.Sqrt(d);
+                double x1 = (-b + sqrt_d) / (2 * a);
+                double x2 = (-b - sqrt_d) / (2 * a);
+                if (d == 0)
+                    Console.WriteLine($"X = {x1}");
+                else
+                    Console.WriteLine($"X = {x1} ; {x2}");
             }
-            else
+            public static void PythagoreanTheorem()
             {
-                c = a * a + b * b;
-                Console.WriteLine($"hypotenuse square -- {c} | hypotenuse -- {Math.Sqrt(c)}");
+                Console.WriteLine("Type -1 in the value you want to find (It will find c and c^2 by defualt)");
+                Console.Write("a:");
+                double a = double.Parse(Console.ReadLine());
+                Console.Write("b:");
+                double b = double.Parse(Console.ReadLine());
+                Console.Write("c:");
+                double c = double.Parse(Console.ReadLine());
+                if (a == -1)
+                {
+                    a = c * c - b * b;
+                    Console.WriteLine($"hypotenuse square -- {a} | hypotenuse -- {Math.Sqrt(a)}");
+                }
+                else if (b == -1)
+                {
+                    b = c * c - a * a;
+                    Console.WriteLine($"hypotenuse square -- {b} | hypotenuse -- {Math.Sqrt(b)}");
+                }
+                else
+                {
+                    c = a * a + b * b;
+                    Console.WriteLine($"hypotenuse square -- {c} | hypotenuse -- {Math.Sqrt(c)}");
+                }
             }
-        }
-        public static void Circumference()
-        {
-            Console.WriteLine("radius:");
-            double r = double.Parse(Console.ReadLine());
-            if (r < 0) Console.WriteLine("wrong value");
-            else Console.WriteLine($"Circumference -- {2 * Math.PI * r}");
-        }
-        public static void AreaOfCircle()
-        {
-            Console.WriteLine("radius:");
-            double r = double.Parse(Console.ReadLine());
-            if (r < 0) Console.WriteLine("wrong value");
-            else Console.WriteLine($"Area of a circle -- {Math.PI * r * r}");
-        }
-        public static void ParallepipedVolume()
-        {
-            Console.Write("a:");
-            double a = double.Parse(Console.ReadLine());
-            Console.Write("b:");
-            double b = double.Parse(Console.ReadLine());
-            Console.Write("c:");
-            double c = double.Parse(Console.ReadLine());
-            Console.WriteLine($"Parallepiped's Volume -- {a * b * c}");
-        }
-        public static void AmperResVolt()
-        {
-            Console.WriteLine("Type -1 in the value you want to find(It will find voltage by defualt)");
-            Console.Write("I:");
-            double I = double.Parse(Console.ReadLine());
-            Console.Write("R:");
-            double R = double.Parse(Console.ReadLine());
-            Console.Write("U:");
-            double U = double.Parse(Console.ReadLine());
-            if (I == -1) //Пользователь может выберать что ему надо, в зависимости от того какие данные у него есть
+            public static void Circumference()
             {
-                I = U/R;
-                Console.WriteLine($"amperage -- {I}");
+                Console.WriteLine("radius:");
+                double r = double.Parse(Console.ReadLine());
+                if (r < 0) Console.WriteLine("wrong value");
+                else Console.WriteLine($"Circumference -- {2 * Math.PI * r}");
             }
-            else if (R == -1)
+            public static void AreaOfCircle()
             {
-                R =  U/I;
-                Console.WriteLine($"resistance -- {R}");
+                Console.WriteLine("radius:");
+                double r = double.Parse(Console.ReadLine());
+                if (r < 0) Console.WriteLine("wrong value");
+                else Console.WriteLine($"Area of a circle -- {Math.PI * r * r}");
             }
-            else
+            public static void ParallepipedVolume()
             {
-                U = I*R;
-                Console.WriteLine($"voltage -- {U}");
+                Console.Write("a:");
+                double a = double.Parse(Console.ReadLine());
+                Console.Write("b:");
+                double b = double.Parse(Console.ReadLine());
+                Console.Write("c:");
+                double c = double.Parse(Console.ReadLine());
+                Console.WriteLine($"Parallepiped's Volume -- {a * b * c}");
+            }
+            public static void AmperResVolt()
+            {
+                Console.WriteLine("Type -1 in the value you want to find(It will find voltage by defualt)");
+                Console.Write("I:");
+                double I = double.Parse(Console.ReadLine());
+                Console.Write("R:");
+                double R = double.Parse(Console.ReadLine());
+                Console.Write("U:");
+                double U = double.Parse(Console.ReadLine());
+                if (I == -1)
+                {
+                    I = U / R;
+                    Console.WriteLine($"amperage -- {I}");
+                }
+                else if (R == -1)
+                {
+                    R = U / I;
+                    Console.WriteLine($"resistance -- {R}");
+                }
+                else
+                {
+                    U = I * R;
+                    Console.WriteLine($"voltage -- {U}");
+                }
             }
         }
     }
-}
+
--- Labs216/Anisimov/DivisionByInterval.cs
+++ Labs216/Anisimov/DivisionByInterval.cs
@@ -6,7 +6,31 @@ namespace Labs216.Anisimov
 {
     public class DivisionByInterval
     {
-        //Use !Program.cs to run
+        /*
+        static void Main(string[] args)
+        {
+            DivisionByInterval.divisionbyinterval();
+        }
+        */
+
+        /*
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            double num = double.Parse(Console.ReadLine());
+            Console.WriteLine("First number of interval:");
+            int first = int.Parse(Console.ReadLine());
+            Console.WriteLine("Last numver of interval:");
+            int last = int.Parse(Console.ReadLine());
+            double[] res = new double[last - first + 1];
+            res = DivisionByInterval.divisionbyinterval(num, first, last);
+            for (int i = first; i < last + 1; i++)
+            {
+                if (i == 0) continue;
+                Console.WriteLine($"{num}/{i} -- {res[i - first]}");
+            }
+        }
+        */
         public static void divisionbyinterval()
         {
             Console.WriteLine("Enter number");
