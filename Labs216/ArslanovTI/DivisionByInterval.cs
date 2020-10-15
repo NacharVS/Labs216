c8e3679501a844cf720580664a354971f9bcf24d
FZQXLOL <timurrikkert@yandex.ru>
2020-10-15 14:43:31 +0300

    Division by interval method by Arslanov Timur

--- Labs216/ArslanovTI/DivisionByInterval.cs
+++ Labs216/ArslanovTI/DivisionByInterval.cs
@@ -1,11 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+//Создайте метод, который находит результат деления чисел из интервала чисел
 namespace Labs216.ArslanovTI
 {
+    
     class DivisionByInterval
     {
-        
+        public static void ollo(string[] args)
+        {
+            Console.WriteLine("Введите число, на которое делить");
+            double a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите первое число интервала");
+            double b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число интервала");
+            double c = int.Parse(Console.ReadLine());
+            for (double i = b; i > c; i++)
+            {
+                double result = a / i;
+                Console.WriteLine(result);
+            }
+
+        }
+
+
+        }
     }
 }
--- Labs216/Program.cs
+++ Labs216/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Labs216.ArslanovTI;
+using System;
 
 namespace Labs216
 {
@@ -6,9 +7,7 @@ namespace Labs216
     {
         static void Main(string[] args)
         {
-            Console.Write("Ente Ur Name");
-            string a = Console.ReadLine();
-            Console.WriteLine("Hello, "+a);
+            DivisionByInterval
 
         }
     }
