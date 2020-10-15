ec872a94fa45811500606a47e4efb9a293810cdf
Zulfia <zulfianabiullina4998@.com>
2020-10-15 15:10:15 +0300

    nachala lab2

---
+++ Labs216/LetterFix.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216
+{
+    class LetterFix
+    {
+        public static void LetterFix1()
+        {
+            
+            {
+                 public string _name;
+                    public string _surname;
+    }
+        }
+    }
+}
