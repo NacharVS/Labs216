59034cc9d75edc4ca1a9564f10a4d7dac307c9a8
ZU216 <garipovazuhra4@email.com>
2020-10-15 15:09:44 +0300

    mid fix

---
+++ Labs216/DivisionByInterval.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Garipova216
+{
+    class DivisionByInterval
+
+    {
+        static public void DivisionByInterval1()
+        {
+            Console.WriteLine("Enter a number-");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Division from what number-");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Division from what number-");
+            double c = double.Parse(Console.ReadLine());
+            for (double d = b; d <= c; d++)
+            {
+                if (d != 0)
+                {
+                    double x = a / d;
+                    Console.WriteLine($"{a} divided by {d} = {x}");
+                }   
+            }
+        }
+    }
+}
