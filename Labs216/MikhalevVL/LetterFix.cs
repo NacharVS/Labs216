60af3a8f1b91815df4f9ffaba52f2b7e13a3e664
ForestWyvern <familkas8@gmail.com>
2020-10-16 12:35:20 +0300

    Fixed LetterFix

--- Labs216/MikhalevVL/LetterFix.cs
+++ Labs216/MikhalevVL/LetterFix.cs
@@ -1,14 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Labs216.MikhalevVL
 {
     class LetterFix
     {
-        static public void Letterfix()
-        { 
+        private string name_string;
+        private string surname_string;
 
+        public string Name
+        {
+            get { return name_string; }
+            set
+            {
+                name_string = $"{value[0].ToString().ToUpper() + value.Substring(1)}";
+            }
+        }
+        public string Surname
+        {
+            get { return surname_string; }
+            set
+            {
+                surname_string = $"{value[0].ToString().ToUpper() + value.Substring(1)}";
+            }
         }
     }
 }
