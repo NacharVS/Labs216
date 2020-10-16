a15863f0ac4ed29c7b1a9248f2a92b50314f0e4b
Azaliya <amalkaazaliya@gmail.com>
2020-10-16 12:45:37 +0300

    Teorema Pyphagorian

---
+++ Labs216/GaraevaAF/Exercise3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace Labs216.GaraevaAF
+{
+    class Exercise3
+    {
+        static public void Exercise31()
+        {
+            Console.WriteLine("Solve the Pythagorean theorem");
+            Console.WriteLine("c*c = a*a + b*b");
+            Console.ReadLine();
+            Console.WriteLine("Enter the first leg length");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second leg length");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Now find the length of the hypotenuse");
+            int c = 0;
+            {
+                if (a > 0)
+                    c = (a * a) + (b * b);
+                Console.WriteLine(c);
+            }
+        }
+        
+    }
+}
