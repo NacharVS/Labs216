ba36a431c7a563e35d27c21ce9c1a3045778dc8f
Zulfia <zulfianabiullina4998@.com>
2020-10-16 13:12:04 +0300

    2 lab nadejus skoro pojmy

---
+++ Labs216/Nabiullina/LetterFix.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216
+{
+    class Group
+    {
+        public string _name;
+        public string _surname;
+        public Group(string name, string surname)
+        {
+            _name = name;
+            _surname = surname;
+            
+
+        }
+
+    }
+   
+    class LetterFix
+    {
+        public static void LetterFix1(Group[] list)
+        {
+
+        }
+
+
+
+    }
+}
--- Labs216/Program.cs
+++ Labs216/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using
+
 using Labs216.Nabiullina;
 
 namespace Labs216
@@ -10,7 +10,20 @@ namespace Labs216
     {
         static void Main(string[] args)
         {
-            DivisionByInterval.DivisionByInterval1();
+            Group[] group = new Group[5];
+            group[0] = new Group("Zina", "Galinganova");
+            group[1] = new Group("azamat", "Colovejov");
+            group[2] = new Group("Fedor", "Cafronov");
+            group[3] = new Group("maria", "Gabutdinov");
+            group[4] = new Group("alesya", "Zajchuk");
+            
+            Console.WriteLine(group[0]._name[0] = group[0]._name[0].ToString().ToUpper());
+            Console.WriteLine(group[1]._name[0].ToString().ToUpper());
+            Console.WriteLine(group[2]._name[0].ToString().ToUpper());
+            Console.WriteLine(group[3]._name[0].ToString().ToUpper());
+            Console.WriteLine(group[4]._name[0].ToString().ToUpper());
+
+            LetterFix.LetterFix1(group);
 
         }
     }
