7d01f084c1cd0627b2fa237c73fc5a94d0665bf2
DSeregaV <ddmmiittrriieevv2003@gmail.com>
2020-10-17 12:37:38 +0300

    Devisioninterval

---
+++ Labs216/Dmitriev/devisioninterval.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Dmitriev
+{
+    class Devisioninterval
+    {
+        static public void devisioninterval()
+        {
+            Console.WriteLine("Введите number");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите start");
+            int start = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите end");
+            int end = int.Parse(Console.ReadLine());
+            for (int k = start; k <= end; k++) 
+            {
+                if (k == 0) continue;
+                int devision = number / k;
+                Console.WriteLine($"{number} /{k}={devision} ");
+
+
+
+            }
+        }
+    }
+}
