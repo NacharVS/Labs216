55629d8f55c75fb345facc7b65d756c6c920e39f
shay407 <world.deep@mail.ru>
2020-10-17 13:19:58 +0300

    Division byInterval

--- Labs216/Shakirov/DivisionByInterval.cs
+++ Labs216/Shakirov/DivisionByInterval.cs
@@ -6,5 +6,30 @@ namespace Labs216.Shakirov
 {
     class DivisionByInterval
     {
+            private static void Main(string[] args)
+            {
+                Console.WriteLine("Enter number");
+                int number = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter begin interval");
+                int first = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter end interval");
+                int second = int.Parse(Console.ReadLine());
+
+                for (int i = first; i <= second; i++)
+                {
+                    double devision = number / i;
+                    Console.WriteLine($"Результат деления {number} на {i} = {devision}");
+                }
+            }
+
+        }
     }
-}
+
+
+
+
+
+
+
