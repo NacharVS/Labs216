f19239402f96b8f8a255bc86f3e69cba524e7422
bu1at <gabdulkhaev03@mail.ru>
2020-10-17 13:00:24 +0300

    nachal string operation

---
+++ Labs216/Gabdulkhaev/String Operation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Gabdulkhaev
+{
+    class StringOperation
+    {
+        public static void SOP()
+        {
+            Console.WriteLine("Введите слово");
+            string slovo = Console.ReadLine();
+            int kolvo = slovo.Length;
+            for (int a = 0; a < kolvo; a = a + 1)
+                string chetnie = slovo.Substring(a, 1);
+            Console.WriteLine(chetnie);
+        }
+    }
+}
