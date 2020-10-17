4af8fda94ffe76947978f5c94e18c8d4481bc683
Mir-ax <mirasov.egor@mail.ru>
2020-10-17 12:46:50 +0300

    First task

--- Labs216/MirasovEG/StringOperations.cs
+++ Labs216/MirasovEG/StringOperations.cs
@@ -6,5 +6,25 @@ namespace Labs216.MirasovEG
 {
     class StringOperations
     {
+        public static void OddEven(string s)
+        {
+            char[] str = s.ToCharArray();
+            
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (i % 2 == 0)
+                    Console.Write(str[i]);
+            }
+
+            Console.WriteLine();
+            for (int i = 0; i < str.Length; i++)
+            {
+
+                if (i % 2 != 0)
+                    Console.Write(str[i]);
+                
+            }
+
+        }
     }
 }
--- Labs216/Program.cs
+++ Labs216/Program.cs
@@ -7,24 +7,10 @@ namespace Labs216
     {
         static void Main(string[] args)
         {
-            LetterFix[] std = new LetterFix[5];
-            for (int i = 0; i < 5; i++)
-            {
-                std[i] = new LetterFix();
+            Console.WriteLine("Enter string:");
+            string s = Console.ReadLine();
+            StringOperations.OddEven(s);
 
-                Console.WriteLine("Enter Name:");
-                std[i].Name = Console.ReadLine();
-
-                Console.WriteLine("Enter Surname:");
-                std[i].Surname = Console.ReadLine();
-            }
-
-            Console.WriteLine();
-
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine($"Student number {i+1}: {std[i].Name} | {std[i].Surname}");
-            }
         }
     }
 }
