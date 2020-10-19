c01c255a918fc30ad7f0524a59dcefd577eb4367
ForestWyvern <familkas8@gmail.com>
2020-10-19 19:32:39 +0300

    Update

--- Labs216/Program.cs
+++ Labs216/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Labs216.MikhalevVL;
-using System;
 
 namespace Labs216
 {
@@ -7,26 +6,8 @@ namespace Labs216
     {
         static void Main(string[] args)
         {
-           
-                LetterFix[] student = new LetterFix[5];
-                for (int i = 0; i < 5; i++)
-                {
-                    student[i] = new LetterFix();
-
-                    Console.WriteLine("Enter Name:");
-                    student[i].Name = Console.ReadLine();
-
-                    Console.WriteLine("Enter Surname:");
-                    student[i].Surname = Console.ReadLine();
-                }
-
-                Console.WriteLine();
-
-                for (int i = 0; i < 5; i++)
-                {
-                    Console.WriteLine($"Student {i+1} -- {student[i].Name} {student[i].Surname}");
-                }
-            }
+            
         }
-
     }
+
+}
