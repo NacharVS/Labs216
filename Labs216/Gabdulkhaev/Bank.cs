743925dc38d7748ad71730ec144a609693702fbe
bu1at <gabdulkhaev03@mail.ru>
2020-10-21 14:57:50 +0300

    nachal bank

---
+++ Labs216/Gabdulkhaev/Bank.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Gabdulkhaev
+{
+    class bank
+    {
+        // Имя Фамилия\
+        // ID клиента
+        // Процент
+        // Кол-во денег на счету
+        // сгенерировать
+    }
+}
