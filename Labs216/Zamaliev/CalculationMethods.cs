b0b5dd837e9ec8faee0811d1aaed9a19f280d359
Ainur-ctrl <zamaliev.aj@yandex.com>
2020-10-21 15:11:01 +0300

    nedodelal calculator

--- Labs216/Zamaliev/CalculationMethods.cs
+++ Labs216/Zamaliev/CalculationMethods.cs
@@ -6,5 +6,30 @@ namespace Labs216.Zamaliev
 {
     class CalculationMethods
     {
+        public static void CS()
+        {
+            while (true)
+            {
+                Console.WriteLine("Дискриминат-1");
+                Console.WriteLine("Теорема Пифагора-2");
+                Console.WriteLine("Длина окружности-3");
+                Console.WriteLine("Площадь круга-4");
+                Console.WriteLine("Объем параллелепипеда-5");
+                Console.WriteLine("Сила тока/Напряжение/Сопротивление через друг друга");
+                int a = Convert.ToInt32(Console.ReadLine());
+                switch (a)
+                {
+                    case 1:
+                        Console.WriteLine("Нахождение дискримината");
+                        Console.WriteLine("Введите a");
+                        int one = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Введите b");
+                        int two = Convert.ToInt32(Console.ReadLine());
+                }
+
+            }
+        }
+
+        
     }
 }
