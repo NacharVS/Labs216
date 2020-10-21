c98f15afbf22da9393dcc9e3165a6abe178f96b8
shay407 <world.deep@mail.ru>
2020-10-21 16:01:32 +0300

    + firstname + name

--- Labs216/Shakirov/LetterFix.cs
+++ Labs216/Shakirov/LetterFix.cs
@@ -6,6 +6,20 @@ namespace Labs216.Shakirov
 {
     class LetterFix
     {
+        private string _firstname;
+        private string _name;
 
+        public Student(string firstname, string name)
+        {
+            _firstname = firstname;
+            _name = name;
+        }
+
+        public string firstname
+        {
+            get { return _firstname}
+
+            set
+        }
     }
 }
