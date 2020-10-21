dd8300dd7486d8f7b8d832af962cc911d4d68be1
DSeregaV <ddmmiittrriieevv2003@gmail.com>
2020-10-21 14:27:29 +0300

    begin

---
+++ Labs216/Dmitriev/calculates.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Dmitriev
+{
+    class calculates
+    {
+        static public void QuadraticEquation(int x,int y,int z)
+        { 
+            int D = (y * y) - (4 * x * z);
+            int x1= (-y + Convert.ToInt32(Math.Sqrt(D))) / (2 * x);
+            int x2= (-y - Convert.ToInt32(Math.Sqrt(D))) / (2 * x);
+            Console.WriteLine($"x1={x1}|")
+
+        }
--- Labs216/Program.cs
+++ Labs216/Program.cs
@@ -7,7 +7,7 @@ namespace Labs216
     {
         static void Main(string[] args)
         {
-            Devisioninterval.devisioninterval();
+            calculates.QuadraticEquation(2, 9, 6);
 
         }
     }
