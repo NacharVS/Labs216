6ee90f4afd0ca3140393303bf19ee98b4082c59d
Mir-ax <mirasov.egor@mail.ru>
2020-10-21 13:58:26 +0300

    First commit

---
+++ Labs216/MirasovEG/Bank.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.MirasovEG
+{
+    class Bank
+    {
+        private string _name;
+        private string _ID;
+        private string _surname;
+        private int stavka;
+        private int schet;
+
+        
+
+    }
+}
