7fa51cf6d6a48c47434ab91703274e97f3e6f1fc
Zulfia <zulfianabiullina4998@.com>
2020-10-21 14:26:39 +0300

    Nachala bank

---
+++ Labs216/Nabiullina/BANK.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Nabiullina
+{
+    class BANK
+    {
+        public string _name;
+        public string _surname;
+        public static decimal _stavka;
+        public string _id;
+        public int _schet;
+        public BANK(string name, string surname, decimal stavka, string id, int schet)
+        {
+            _name = name;
+            _surname = surname;
+            _stavka = stavka;
+            _id = id;
+            _schet = schet;
+
+        }
+    }
+}
