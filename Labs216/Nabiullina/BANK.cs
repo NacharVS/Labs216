a50fc5ef9a2cfa78bc817b48a19174792c0689fe
r3dinc <r3dinc@yandex.ru>
2020-10-21 14:53:49 +0300

    bank fix

--- Labs216/Nabiullina/BANK.cs
+++ Labs216/Nabiullina/BANK.cs
@@ -6,11 +6,12 @@ namespace Labs216.Nabiullina
 {
     class BANK
     {
-        public string _name;
-        public string _surname;
-        public static decimal _stavka;
-        public string _id;
-        public int _schet;
+        private string _name;
+        private string _surname;
+        private static decimal _stavka;
+        private string _id;
+        private int _schet;
+
         public BANK(string name, string surname, decimal stavka, string id, int schet)
         {
             _name = name;
