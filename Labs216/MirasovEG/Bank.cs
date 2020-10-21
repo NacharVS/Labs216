8b77d179fbc72b173e889339c7bc96ea1c013dc7
Mir-ax <mirasov.egor@mail.ru>
2020-10-21 20:20:04 +0300

    Finished making a Bank account

--- Labs216/MirasovEG/Bank.cs
+++ Labs216/MirasovEG/Bank.cs
@@ -10,10 +10,10 @@ namespace Labs216.MirasovEG
         private string _name;
         private string _ID;
         private string _surname;
-        private static int stavka = 6;
-        private int _bill;
+        private static int _stavka = 6;
+        private int _account;
         private static int _minPut = 1000;
-        private static int _maxTake = 100000;
+        private static int _maxTake = 10000000;
         private static int count = 0;
 
         public string Name
@@ -30,77 +30,104 @@ namespace Labs216.MirasovEG
 
         public void GenerationID()
         {
-            Bank bank = new Bank();
-            bank._ID = bank.Name + bank.Surname + Bank.count;
-            Bank.count += 1;
+            
+            _ID = Name + Surname + count;
+            count += 1;
         }
 
         public static void StartBank()
         {
             Bank bank = new Bank();
+                
+            Console.WriteLine("Enter Name:");
+            bank.Name = Console.ReadLine();
 
-                Console.WriteLine("Enter Name:");
-                bank.Name = Console.ReadLine();
+            Console.WriteLine("Enter Surname:");
+            bank.Surname = Console.ReadLine();
 
-                Console.WriteLine("Enter Surname:");
-                bank.Surname = Console.ReadLine();
+            bank.GenerationID();
+            Console.WriteLine($"Your ID: {bank._ID}");
 
-                bank.GenerationID();
-
+            Console.WriteLine();
             while (true)
             {
-                Console.WriteLine("Choose actions with the bill (1 - To put money into the account || 2 - Withdraw money from an account )");
+                Console.WriteLine("Choose actions with the bill \n 1 - To put money into the account \n 2 - Withdraw money from an account \n 3 - Calculate the amount of savings for several years ahead");
                 int choice = int.Parse(Console.ReadLine());
 
                 switch (choice)
                 {
                     case 1:
-                        Put();
+                        bank.Put();
                         break;
 
                     case 2:
-                        Take();
+                        bank.Take();
+                        break;
+
+                    case 3:
+                        bank.Calculate();
                         break;
 
                 }
                 Console.WriteLine("Want to repeat the operation selectin (Y/N)");
                 string a = Console.ReadLine();
-                if (a == "N") break;
+                if (a == "N" || a =="n") break;
 
-                Console.WriteLine("Want to know the account size (Y/N)");
-                string b = Console.ReadLine();
-                if (b == "Y")
-                    Console.WriteLine(bank._bill);
+                Console.WriteLine();
             }
-            
-
-                
         }
 
-        public static void Put()
+        public void Put()
         {
-            Bank bank = new Bank();
+            
             Console.WriteLine("How much do you want to deposit");
             int value = int.Parse(Console.ReadLine());
-            if(value < Bank._minPut)
-                Console.WriteLine($"Value less then minimum ({Bank._minPut})");
+
+            if(value < _minPut)
+                Console.WriteLine($"Value less then minimum ({_minPut})");
             else
-            bank._bill += value; 
+                _account += value;
+
+            Console.WriteLine($"On your account: {_account}");
+            Console.WriteLine();
         }
-        public static void Take()
+        public void Take()
         {
-            Bank bank = new Bank();
+            
             Console.WriteLine("How much do you want withdraw from the bill");
             int value = int.Parse(Console.ReadLine());
-            if (value > Bank._maxTake)
-                Console.WriteLine($"Value is greater then maximum ({Bank._maxTake})");
 
-            if (bank._bill > value)
-                bank._bill -= value;
+            if (value > _maxTake)
+                Console.WriteLine($"Value is greater then maximum ({_maxTake})");
+
+            if (_account > value)
+                _account -= value;
             else
                 Console.WriteLine("Insufficient funds on the bill");
 
+            Console.WriteLine($"On your account: { _account}");
+            Console.WriteLine();
         }
 
+        public void Calculate()
+        {
+            if (_account == 0)
+                Console.WriteLine("First you need to deposit money to your account");
+
+            else
+            {
+                Console.WriteLine("How many years to calculate a Bank account");
+                int year = int.Parse(Console.ReadLine());
+                double calculate = 0;
+
+                for (int i = 0; i < year; i++)
+                {
+                    calculate += ((_account + calculate) * _stavka) / 100;
+                }
+                
+                Console.WriteLine($"Amount of savings in the account after {year} years: {calculate + _account}");
+            }
+            Console.WriteLine();
+        }
     }
 }
