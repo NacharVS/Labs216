8283970eb7e1c1c4c567e6674e7e62b2973cc171
Ivanov08 <antonivanov08012003@mail. ru>
2020-10-22 14:31:18 +0300

    DivisionByInterval

--- Labs216/Faskhutdinov/DIvisionByInterval.cs
+++ Labs216/Faskhutdinov/DIvisionByInterval.cs
@@ -6,5 +6,26 @@ namespace Labs216.Faskhutdinov
 {
     class DIvisionByInterval
     {
+        static void Main(string[] args)
+        {
+            DivisionByInterval.divisionbyinterval();
+        }
+    }
+    public class DivisionByInterval
+    {
+        public static void divisionbyinterval()
+        {
+            Console.WriteLine("Enter number");
+            double numb = double.Parse(Console.ReadLine());
+            Console.WriteLine("First number of interval:");
+            double first = double.Parse(Console.ReadLine());
+            Console.WriteLine("Last numver of interval:");
+            double last = double.Parse(Console.ReadLine());
+            for (double i = first; i < last + 1; i++)
+            {
+                if (i == 0) continue;
+                Console.WriteLine($"{numb}/{i} -- {numb / i}");
+            }
+        }
     }
 }
