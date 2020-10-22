5fe7ba989c4f99834cedb763b72e8e2ad66b1272
KateTop216 <k_top683@mail.ru>
2020-10-22 15:08:09 +0300

    The second task of the first practical session. Beginning

--- Labs216/Program.cs
+++ Labs216/Program.cs
@@ -23,3 +23,30 @@ namespace Labs216
         }
     }
 }
+
+namespace Labs216
+{
+    class DivisionByInterval2
+    //Square formula
+    //Pythagorean theorem
+    //circumFerence
+    //area of a circle
+    //parallepiped's Volume
+    //amperage/resistence/voltage
+    {
+        static public void Main(string[] args);
+        {
+           double FirstValue, SecoundeValue, ThirdValue; 
+           string action;
+           Console.WriteLine("Введите число 1");
+           FirsteValue = double.Parse(Console.ReadLine());
+           Console.Write.Line("Введите число 2");
+           SecoundeValue = double.Parse(Console.ReadLine());
+           Console.WriteLine("Введиете формулу: 'a^2 = b^2+c^2' 'c^2 = a^2+b^2' '2PI*R^2' 'PI*R^2' 'A*B*C');
+           action = Console.ReadLine()); 
+
+              switch(action)
+                {
+        }
+    }
+}
