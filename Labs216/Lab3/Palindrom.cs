a6e8b66881cc803e20248b947e4c850e60547790
Azaliya <amalkaazaliya@gmail.com>
2020-10-22 14:40:46 +0300

    polnaya lab3

--- Labs216/Lab3/Palindrom.cs
+++ Labs216/Lab3/Palindrom.cs
@@ -8,7 +8,19 @@ namespace Labs216.Lab3
     {
         static public void Palindrom1()
         {
-          
+            Console.WriteLine("Enter phrase");
+            string phrase = (Console.ReadLine());
+            string palindrom = null;
+            phrase = phrase.Replace(" ", " ").ToLower();
+            for (int i = 0; i < phrase.Length; i++)
+            {
+                palindrom += phrase[phrase.Length - 1 - i];
+            }
+            if (phrase == palindrom)
+                Console.WriteLine("Is an palindrom");
+            else
+                Console.WriteLine("No palindrom");
+
         }
     }
 }
