5c65f69c482507e943745babcb51bceb7aa25d96
bu1at <gabdulkhaev03@mail.ru>
2020-10-22 14:59:45 +0300

    beda s bankom

--- Labs216/Gabdulkhaev/Bank.cs
+++ Labs216/Gabdulkhaev/Bank.cs
@@ -11,5 +11,17 @@ namespace Labs216.Gabdulkhaev
         // Процент
         // Кол-во денег на счету
         // сгенерировать
+        private string _name;
+        private string _surname;
+        private string _id;
+        private static double _rate;
+        private double _paymentAccount;
+        public void SetName(string newName)
+        {
+            newName = newName.Trim();
+            var firstLetter = newName[0];
+            var otherLetters = newName.Remove(0, 1);
+            _name = firstLetter.ToString().ToUpper() + otherLetters;
+        }
     }
 }
