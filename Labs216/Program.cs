fc429ea2131794b2d228e8f7d540b0d8da813da6
KateTop216 <k_top683@mail.ru>
2020-10-23 13:18:06 +0300

    Finished

--- Labs216/Program.cs
+++ Labs216/Program.cs
@@ -4,7 +4,9 @@ namespace Labs216
 {
     class DivisionByIntrval
     {
-        static void Main(string[] args)
+        private static int x;
+
+        private static void Main(string[] args)
         {
           Console.WriteLine("Введите число");
           double a = double.Parse(Console.ReadLine());
@@ -34,19 +36,43 @@ namespace Labs216
     //parallepiped's Volume
     //amperage/resistence/voltage
     {
-        static public void Main(string[] args);
-        {
-           double FirstValue, SecoundeValue, ThirdValue; 
-           string action;
-           Console.WriteLine("Введите число 1");
+        static  void Main(string[] args) 
+        { 
+        double a = Console.ReadLine("Введите число 1");
+        double b = Console.ReadLine("Введите число 2");
+        double c = Console.ReadLine("Введите число 3");
+        double R = Console.ReadLine("Введите число 4");
+        
+           double  //FirstValue, SecoundeValue, ThirdValue;
+        string action;
+        Console.WriteLine("Введите число");
            FirsteValue = double.Parse(Console.ReadLine());
-           Console.Write.Line("Введите число 2");
+        Console.Write.Line("Введите число ");
            SecoundeValue = double.Parse(Console.ReadLine());
-           Console.WriteLine("Введиете формулу: 'a^2 = b^2+c^2' 'c^2 = a^2+b^2' '2PI*R^2' 'PI*R^2' 'A*B*C');
-           action = Console.ReadLine()); 
+            Console.WriteLine("Введиете формулу: ('a^2 = b^2+c^2' 'c^2 = a^2+b^2' '2PI*R^2' 'PI*R^2' 'A*B*C'))
+
+                   action = Console.ReadLine()); 
 
               switch(action)
                 {
+                  case "a^2 = b^2+c^2";
+                   Console.WriteLine("$a^2 = b^2+c^2");
+                    break;
+                  case "c^2 = a^2+b^2";
+                   Console.WriteLine("$c^2 = a^2+b^2 ");
+                    break;
+                  case "2PI*R^2";
+                   Console.WriteLine("2PI*R^2");
+                    break;
+                  case "PI*R^2";
+                   Console.WriteLine("$ PI*R^2");
+                    break;
+                  case "A*B*C";
+                   Console.WriteLine("A*B*C");
+                    break;
+                  defoult:
+                   Console.WriteLine("Ошибка!")
+                }
         }
     }
 }
