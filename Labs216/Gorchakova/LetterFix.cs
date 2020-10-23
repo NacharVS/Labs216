c1a83c9ed27b604456ffae07d6bf6bf611f6fb14
Alyona <wernite1812@gmail.com>
2020-10-23 13:06:29 +0300

    перезалив, надеюсь

--- Labs216/Gorchakova/LetterFix.cs
+++ Labs216/Gorchakova/LetterFix.cs
@@ -3,6 +3,60 @@ namespace Labs216.Gorchakova
 {
     public class LetterFix
     {
+        private string _name;
+        private string _surname;
 
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value == "")
+                {
+                    _name = "No Data";
+                }
+                else
+                    _name = $"{value[0].ToString().ToUpper() + value.Substring(1)}";
+            }
+        }
+        public string Surname
+        {
+            get { return _surname; }
+            set
+            {
+                if (value == "")
+                {
+                    _surname = "No Data";
+                }
+                else
+                    _surname = $"{value[0].ToString().ToUpper() + value.Substring(1)}";
+
+            }
+        }
+
+
+        public static void LetterFixMethods()
+        {
+            LetterFix[] std = new LetterFix[5];
+            for (int i = 0; i < 5; i++)
+            {
+                std[i] = new LetterFix();
+
+                Console.WriteLine("Enter Name:");
+                std[i].Name = Console.ReadLine();
+
+                Console.WriteLine("Enter Surname:");
+                std[i].Surname = Console.ReadLine();
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Student number {i + 1}: {std[i].Name} | {std[i].Surname}");
+            }
+        }
     }
+
 }
+
--- Labs216/Gorchakova/StringOperations.cs
+++ Labs216/Gorchakova/StringOperations.cs
@@ -3,8 +3,53 @@ namespace Labs216.Gorchakova
 {
     public class StringOperations
     {
-        public StringOperations()
-        {
+        
+            public static void AddEven(string s)
+            {
+                Console.Write("Add characters in the string: ");
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (s[i] == ' ')
+                        continue;
+                    if (i % 2 == 0)
+                        Console.Write(s[i]);
+                }
+
+                Console.WriteLine();
+
+                Console.Write("Even characters in the string: ");
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (s[i] == ' ')
+                        continue;
+                    if (i % 2 != 0)
+                        Console.Write(s[i]);
+
+                }
+            }
+
+            public static void WordCount(string s)
+            {
+                string[] words = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                Console.WriteLine($"Number of words per line = {words.Length}");
+            }
+
+
+            public static void Palindrome(string s)
+            {
+                s = s.Replace(" ", "");
+                int count = 0;
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (s[i] == s[s.Length - 1 - i])
+                        count += 1;
+                }
+                if (count == s.Length)
+                    Console.WriteLine("This string is a palindrome");
+                else
+                    Console.WriteLine("This string is not a palindrome");
+            }
         }
     }
 }
