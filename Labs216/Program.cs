a7469c010964a280133b085b8f4f988d7f4b8e36
N <161905@mck-ktits.ru>
2020-10-24 12:28:22 +0300

    fix

--- Labs216/Program.cs
+++ Labs216/Program.cs
@@ -4,19 +4,8 @@ namespace Labs216
 {
     class DivisionByIntrval
     {
-        static public void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Введите любое число");
-            double a = double.Parse(Console.ReadLine());
-            Console.WriteLine("Введите первое число интервала");
-            double first = double.Parse(Console.ReadLine());
-            Console.WriteLine("Введите последнее число интервала");
-            double last = double.Parse(Console.ReadLine());
-            for (double f = first; f <= last; f++)
-            {
-                if (f == 0) continue;
-                Console.WriteLine($"{a} / {f} --{a / f};
-            }
         }
     }
 }
