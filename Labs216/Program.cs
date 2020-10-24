6f87684470f3269a1ae2fee617f7b054aab2b1ff
N <161905@mck-ktits.ru>
2020-10-24 12:38:22 +0300

    DivisionByInteval

--- Labs216/Program.cs
+++ Labs216/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Labs216.Vail;
 
 namespace Labs216
 {
@@ -6,6 +7,7 @@ namespace Labs216
     {
         static void Main(string[] args)
         {
+            DivisionByInterval.Run();
         }
     }
 }
---
+++ Labs216/Vail/DivisionByInterval.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Vail
+{
+    class DivisionByInterval
+    {
+        static public void Run()
+        {
+            Console.WriteLine("Введите любое число");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите первое число интервала");
+            double first = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите последнее число интервала");
+            double last = double.Parse(Console.ReadLine());
+            for (double f = first; f <= last; f++)
+            {
+                if (f == 0) continue;
+                Console.WriteLine($"{a} / {f} --{a / f}");
+            }
+        }
+    }
+}
