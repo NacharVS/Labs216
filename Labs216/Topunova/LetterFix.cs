aedf359e108f8146c7f2bf92ec37b2e12128598b
KateTop216 <k_top683@mail.ru>
2020-10-27 12:44:24 +0300

    Practical work 2-start

---
+++ Labs216/Topunova/LetterFix.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Topunova
+{
+    class LetterFix
+    {
+      static void Main(string[] args)
+      {
+            string[] s = new string[s] { "ivanov ivan", "petrov petr", "antonov anton", "pavlov pavel", "egorov egor" };
+            for (int i = 0; i <= s.Length; i++)
+            {
+                s[i] = ((string) s [i].Remove(1, s [i].Length - 1)).ToUpper() + s[i].Remove(0, 1);
+                Console.WriteLine($"s[i]");
+            }
+      }
+       
+    }
+}
