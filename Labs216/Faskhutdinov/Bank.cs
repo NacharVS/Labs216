26de207e16d032c00d583fb620d8931707a1a78d
Ivanov08 <antonivanov08012003@mail. ru>
2020-10-28 14:38:02 +0300

    еппеп

---
+++ Labs216/Faskhutdinov/Bank.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Garipova
+{
+    class Bank
+    {
+        private string _name;
+        private string _surname;
+        private string _id;
+        private static double _rate;
+        private double _paymentAccount;
+
+        public void SetName(string newName)
+        {
+            newName = newName.Trim();
+            var firstLetter = newName[0];
+            var otherLetter = newName.Remove(0, 1);
+            _name = (firstLetter.ToString().ToUpper() + otherLetter);
+        }
+
+
+    }
+}
