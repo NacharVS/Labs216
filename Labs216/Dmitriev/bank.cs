a48e8c956152f33f5d2601485dbf4e29e6f0f593
DSeregaV <ddmmiittrriieevv2003@gmail.com>
2020-10-28 14:55:17 +0300

    bank na4alo

---
+++ Labs216/Dmitriev/bank.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Dmitriev
+{
+    class Bank
+    {
+        public static void bankomat()
+        {
+            Bank acc1 = new Bank();
+            Console.WriteLine("What do you want: 1 -- вывести деньги || 2 -- вложить деньги || 3 -- проверка счета  || 4 -- расчитать депозит");
+        }
+        private string _name;
+        private string _surname;
+        private string _id;
+        private static int stavka = 9;
+        private double _account;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Surname
+        {
+            get { return _surname; }
+            set { _surname = value; }
+        }
+        public string id
+        {
+            get { return _id; }
+        }
+        public string accound
+        {
+            get { return accound; }
+            set { accound = value; }
+        }
+        public Bank()
+        {
+            Console.WriteLine("введите имя");
+            _name = Console.ReadLine();
+            Console.WriteLine("введите фамилию");
+            _surname = Console.ReadLine();
+            _id = _name + _surname ;
+            
+            Console.WriteLine($"здраствуйте, {_name} {_surname}, ваш счет, ваш id  {_id} у вас  {_account} ");
+        }
+    }
+}
+    
+    
--- Labs216/Program.cs
+++ Labs216/Program.cs
@@ -7,7 +7,7 @@ namespace Labs216
     {
         static void Main(string[] args)
         {
-            Run.LetterFix();
+            Bank.bankomat();
 
         }
     }
