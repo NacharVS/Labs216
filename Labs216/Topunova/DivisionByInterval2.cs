1bc437f3417443c4050e64e831311e0e8457db74
Kate.Top216 <k_top683@mail.ru>
2020-10-30 00:02:20 +0300

    Practical work 2-getting started(perepisala iz programs)

---
+++ Labs216/Topunova/DivisionByInterval2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Topunova
+{
+    class DivisionByInterval2
+    {
+        //square formula
+        //Pythagorean theorem
+        //cicrumFerence
+        //area of a circle 
+        //parallepiped's volume
+        //amperage/resistence/voltage
+
+
+        static void Main(string[] args)
+        {
+            double a = Console.ReadLine("Введите число 1");
+            double b = Console.ReadLine("Введите число 2");
+            double c = Console.ReadLine("Введите число 3");
+            double d = Console.ReadLine("Введите число 4");
+            string action;
+            Console.WriteLine("Введите число");
+            firsteValue = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число 2");
+            SecoundeValue = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите формулу: "",")
+        }
+    }
+}
