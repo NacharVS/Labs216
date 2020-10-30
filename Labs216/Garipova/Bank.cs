07807d56fb1738a4f6b5784560f7ab52d55ceb81
ZU216 <garipovazuhra4@email.com>
2020-10-30 20:20:19 +0300

    minor fix

--- Labs216/Garipova/Bank.cs
+++ Labs216/Garipova/Bank.cs
@@ -1,24 +1,25 @@
-﻿namespace Labs216.Garipova
-{
-    struct Bank1
+﻿using System;
+
+namespace Labs216.Garipova
+{ 
+    class Bank
     {
         private string _name;
         private string _surname;
         private string _id;
         private static double _rate = 9;
-        private int _Account;
         private static int count = 0;
-        private int _SetDate;
-        private static int _Max = 100000;
-        private static int _Min = 10000;
-        public Bank1(string setname, string setsurname, string setid, int setaccount, int setdate)
-        {
-           _name = name;
-            _surname = surname;
+        private static int _MaxTake = 10000;
+        private static int _MinPut = 1000;
+       private int _year { get; set; }
+        private int _month { get; set; }
 
-        }
-        class Bank
+            public void Getinfo()
         {
+            Console.WriteLine($"Вас зовут:{_name} {_surname }");
+            Console.WriteLine($"Ваш id :{_id }");
+            Console.WriteLine($"{_rate }");
+        }
         public void SetName(string newName)
         {
             newName = newName.Trim();
@@ -33,15 +34,74 @@
             var Lettter = newsurname.Remove(0, 1);
             _surname = (Letter.ToString().ToUpper() + Lettter);
         }
-        public string SetId(string newId)
+            public string Id(string newId)
+            {
+                _id = _name + _surname + count;
+                count += 1;
+                return _id;
+            }
+
+
+
+        public static void Start()
         {
-            _id = name + _surname + count;
-            count += 1;
-        }
-        public double Account()
-        {
-      
-        }
+                Bank bank = new Bank();
+                Console.WriteLine("");
+                bank._name = Console.ReadLine();
+                Console.WriteLine("");
+                bank._surname  = Console.ReadLine();
+
+                Console.WriteLine();
+
+                Console.WriteLine("");
+                bank._year = int.Parse(Console.ReadLine());
+                Console.WriteLine("");
+                bank._month = int.Parse(Console.ReadLine());
+
+                int now = DateTime.Now.Year;
+                int month = DateTime.Now._month;
+
+                if (now - bank._year >= 14 && month > bank._month)
+                {
+                    Console.WriteLine("Access is allowed");
+
+                    while (true)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("");
+                        int choice = int.Parse(Console.ReadLine());
+
+                        switch (choice)
+                        {
+                            case "1":
+                                Console.WriteLine("");
+                                double with = double.Parse(Console.ReadLine());
+                                if (_rate > with)
+                                {
+                                    Console.WriteLine("-");
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"{_rate -= with} money");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("{_rate} money");
+                                }
+                            case "2":
+                                Console.WriteLine("Minput 1000");
+                                double put = double.Parse(Console.ReadLine());
+                                if (put > 1000)
+                                {
+                                    Console.WriteLine("-");
+                                }
+                                else { Console.WriteLine($"{_rate += put} money"); }
+                                break;
+
+                        }
+
+
+            }
 
     }
 }
