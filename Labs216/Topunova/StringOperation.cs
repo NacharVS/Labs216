2687b179968437f7be403e3f861a8592046d904a
Kate.Top216 <k_top683@mail.ru>
2020-10-30 22:44:46 +0300

    The end StringOperation

--- Labs216/Topunova/StringOperation.cs
+++ Labs216/Topunova/StringOperation.cs
@@ -1,7 +1,24 @@
-﻿namespace Labs216.Topunova
+﻿using System;
+
+namespace Labs216.Topunova
 {
     class StringOperation
     {
-        console/
+        public static void Palindrome(string a)
+        {
+            a = a.Replace(" ", "");
+            int count = 0;
+                for (int i = 0; i < a.Length; i++);
+                {
+                if (a[i] == s.Length[string.Length - 1 - i])
+                    count += 1;
+                }
+                {
+                if (count == s.Length)
+                    Console.WriteLine("String-Palindrome");
+                else
+                    Console.WriteLine("String-not Palindrome");
+                }
+        }
     }
 }
