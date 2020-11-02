f3295baa57033f2ab90e857f3b55547176b885d7
Kate.Top216 <k_top683@mail.ru>
2020-11-02 21:48:17 +0300

    End of the second task

--- Labs216/Topunova/StringOperation.cs
+++ Labs216/Topunova/StringOperation.cs
@@ -8,17 +8,27 @@ namespace Labs216.Topunova
         {
             a = a.Replace(" ", "");
             int count = 0;
-                for (int i = 0; i < a.Length; i++);
-                {
+            for (int i = 0; i < a.Length; i++) ;
+            {
                 if (a[i] == s.Length[string.Length - 1 - i])
                     count += 1;
-                }
-                {
+            }
+            {
                 if (count == s.Length)
                     Console.WriteLine("String-Palindrome");
                 else
                     Console.WriteLine("String-not Palindrome");
-                }
+            }
+        }
+        {
+        public static void chtoto()
+            Console.WriteLine("Что-то там, без разницы что");
+            string[] textMass;
+            string texr = Console.ReadLine();
+            textMass = TupleExtensions.Split(' ');
+            Console.WriteLine("номер слова: ");
+            Console.WriteLine(textMass.Length);
+            Console.ReadLine();
         }
     }
 }
