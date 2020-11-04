78048d87a89398f128d145da76c8971347aa7663
Kate.Top216 <k_top683@mail.ru>
2020-11-04 22:28:29 +0300

    Please help me finish, I don't know how to do it.

--- Labs216/Topunova/Bank.cs
+++ Labs216/Topunova/Bank.cs
@@ -12,6 +12,12 @@ namespace Labs216.Topunova
         private static double _rate;
         private double _paymentAccount;
 
+        public void ClientInfo()
+        {
+            Console.WriteLine("Введите: {_name}, {_surname}");
+            Console.WriteLine("Введите: {_id}");
+            Console.WriteLine("const = _rate");
+        }
         public void SetName(string newName)
         {
             _name = newName.Trim();
@@ -19,12 +25,42 @@ namespace Labs216.Topunova
             var otherLetter = newName.Remove(0, 1);
             _name = (firstLetter.ToString().ToUpper() + otherLetter);
         }
+        public void Setsurname(string newsurname)
         {
+            newsurname = newsurname.Trim();
+            var firstLatter = newsurname[0];
+            var otherLatter = newsurname(0, 1);
+            newsurname = (firstLatter.ToString().ToUpper() + otherLatter);
         }
-    {
-    }
-{
+        public void SetId(string newId)
+        {
+            newId = newId.Trim();
+            var firstLatter = newId[0];
+            var otherLatter = newId(0, 1);
+            newId = (firstLatter.ToString().ToUpper() + otherLatter);
+        }
+        public static void Go()
+        {
+            Bank bank = new Bank();
+            Console.WriteLine("Введите: ");
+            bank._name = Console.ReadLine();
+            Console.WriteLine();
+            bank._surname = Console.ReadLine();
 
-}
+            Console.WriteLine();
+
+            Console.WriteLine("Введите: ");
+            double month = double.Parse(Console.WriteLine());
+            double month = double.Parse(Console.ReadLine())
+            Console.WriteLine("Введите: ");
+            double year = double.Parse(Console.WriteLine());
+            double year = double.Parse(Console.ReadLine());
+
+            double now = DateTime.Now.Year;
+            double mounth = DateTime.Now.Month;
+
+            if (now - year  )
+        }
     }
 }
+
