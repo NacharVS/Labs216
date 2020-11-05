369affc93787555c59650c436a6a7ed35e66df50
AlexanderTarskih <jiumohad2017@yandex.ru>
2020-11-05 11:51:02 +0300

    Bank Part I

---
+++ Labs216/Tarskih/Bank.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Tarskih
+{
+    class Bank
+    {
+        private string _name;
+        private string _surname;
+        private int _refund;
+        private static double _rate;
+        private string _id;
+        private double _account;
+
+        public Bank(string name, string surname, int refund, double rate, string id, double account)
+        {
+            _name = name;
+            _surname = surname;
+            _refund = refund;
+            _rate = rate;
+            _id = id;
+            _account = account;
+        }
+
+    }
+}
