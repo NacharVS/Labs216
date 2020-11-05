941fbd09971359373bbdfc0df03407685e872425
AlexanderTarskih <jiumohad2017@yandex.ru>
2020-11-05 12:16:04 +0300

    Bank Part IV

--- Labs216/Tarskih/Bank.cs
+++ Labs216/Tarskih/Bank.cs
@@ -39,8 +39,21 @@ namespace Labs216.Tarskih
         public void SetAccount()
         {
             Console.WriteLine($"На счёте сейчас {_account} валюты");
-            Console.WriteLine(" 1 - Снять со счёта  2 - Положить на счёт");
+            Console.WriteLine($" {1} - Снять со счёта  {2} - Положить на счёт ");
             string b = Console.ReadLine();
+            switch (b)
+            {
+                case "1":
+                    Console.WriteLine("Максимум - 200000 валюты");
+                    double with = double.Parse(Console.ReadLine());
+                    if (_account > with)
+                    {
+                        if (with > 200000)
+                        {
+                            Console.WriteLine($"На вашем счёте: {_account -= with} средств");
+                        }
+                    }
+            }
         }
 
     }
