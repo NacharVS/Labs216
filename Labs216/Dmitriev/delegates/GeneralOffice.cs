fdcfd9b36ce6ae59e632ef46e2a7c0bcaad02080
DSeregaV <ddmmiittrriieevv2003@gmail.com>
2020-11-09 11:08:54 +0300

    na4alo

---
+++ Labs216/Dmitriev/delegates/GeneralOffice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Dmitriev.delegates
+{
+    class GeneralOffice
+    {
+        public static void Massivsort()
+        {
+            int buff;
+            for (int x = 0; x < Array.lenght- 1; x++)
+        }
+    }
+}
