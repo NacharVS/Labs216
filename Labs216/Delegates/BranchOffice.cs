ad81032146968bfe98629f658f94e641be85713f
Zulfia <zulfianabiullina4998@.com>
2020-11-09 11:10:54 +0300

    nachala lalalal

---
+++ Labs216/Delegates/BranchOffice.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Delegates
+{
+    class BranchOffice
+    {
+        static public void Povozrastanijy()
+        {
+            int[] array = new int[5];
+            Random rnd = new Random();
+            int a = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(1, 10);
+                
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i]> array[i++])
+                {
+                    a = array[i];
+                    array[i] = array[i++];
+                    array[i++] = a;
+                }
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                Console.WriteLine(array[i]+ " ");
+
+            }
+        }
+
+        static public void Summmasiv()
+        {
+
+        }
+        
+        static public void Maxelement()
+        {
+
+        }
+    }
+}
--- Labs216/Program.cs
+++ Labs216/Program.cs
@@ -8,18 +8,9 @@ namespace Labs216
 {
     class Program
     {
+        private delegate void
         static void Main(string[] args)
-        {
-
-            BANK account1 = new BANK("Ivan", "Ivanov", 1999 , 6.7 , 100);
-            account1.SetId();
-            account1.SetVozrast();
-
-            account1.SetSchet();
-            account1.SetStavka();
-
-            
-            
+        {               
 
 
 
