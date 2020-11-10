1ea4d285a7a27a6eb86e0e98ea6be72dd404f3ab
shay407 <world.deep@mail.ru>
2020-11-10 13:19:07 +0300

    + 50%

--- Labs216/Shakirov/Bank.cs
+++ Labs216/Shakirov/Bank.cs
@@ -12,12 +12,22 @@ namespace Labs216.Shakirov
         private static double _rate;
         private double _paymentAccount;
 
+        public Bank(string name, string surname, int vozrast, double stavka, string id, double schet)
+        {
+            _name = name;
+            _surname = surname;
+            _vozrast = vozrast;
+            _stavka = stavka;
+            _id = id;
+            _schet = schet;
+        }
         public void SetName(string newName)
         {
-            newName = newName.Trim();
+            newName = newName.Trim;
             var firstLetter = newName[0];
-            var otherLetter = newName.Remove(0, 1);
-            _name = (firstLetter.ToString().ToUpper() + otherLetter);
+            var otherLetters = newName.Remove(0, 1);
+            _name = firstLetter.ToString().ToUpper() + otherLetters;
+
         }
         public void SetSurname(string newSurname)
         {
@@ -26,12 +36,39 @@ namespace Labs216.Shakirov
             var otherLetters = newSurname.Remove(0, 1);
             _surname = firstLetter.ToString().ToUpper() + otherLetters;
         }
-        public static void Bank1()
+        public void SetVozrast ()
         {
-            Console.WriteLine("Добро пожаловать в банк");
-            Console.WriteLine("Ставка банка - 7,3");
-            Console.WriteLine("Введите свои данные");
-            string name, surname = Console.ReadLine();
+            int a = DateTime.Today.Year;
+            _vozrast = a - _vozrast;
+            Console.WriteLine($"Возраст: {_vozrast}");
+        }
+        public void SetSchet ()
+        {
+            Console.WriteLine($"Сейчас на счету: {_schet}");
+            Console.WriteLine(" 1. Снять деньги \n 2. Положить деньги");
+            string b = Console.ReadLine();
+            switch (b)
+            {
+                case "1":
+                    Console.WriteLine("Max 150000");
+                    double with = double.Parse(Console.ReadLine());
+                    if (_schet > with)
+                    {
+                        if (with > 150000)
+                        {
+                            Console.WriteLine("Нет!");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"На вашем счете: {_schet -= with} money");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"На вашем счете: {_schet")
+                    }
+            }
+        }
         }
     }
 }
