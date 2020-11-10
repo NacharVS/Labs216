0fa00c901b37ec477897392ed979d1564905c01f
shay407 <world.deep@mail.ru>
2020-11-10 15:50:19 +0300

    end bank with person

--- Labs216/Shakirov/Bank.cs
+++ Labs216/Shakirov/Bank.cs
@@ -8,9 +8,10 @@ namespace Labs216.Shakirov
     {
         private string _name;
         private string _surname;
+        private int _vozrast;
+        private static double _stavka;
         private string _id;
-        private static double _rate;
-        private double _paymentAccount;
+        private double _schet;
 
         public Bank(string name, string surname, int vozrast, double stavka, string id, double schet)
         {
@@ -23,7 +24,7 @@ namespace Labs216.Shakirov
         }
         public void SetName(string newName)
         {
-            newName = newName.Trim;
+            newName = newName.Trim();
             var firstLetter = newName[0];
             var otherLetters = newName.Remove(0, 1);
             _name = firstLetter.ToString().ToUpper() + otherLetters;
@@ -36,13 +37,13 @@ namespace Labs216.Shakirov
             var otherLetters = newSurname.Remove(0, 1);
             _surname = firstLetter.ToString().ToUpper() + otherLetters;
         }
-        public void SetVozrast ()
+        public void SetVozrast()
         {
             int a = DateTime.Today.Year;
             _vozrast = a - _vozrast;
             Console.WriteLine($"Возраст: {_vozrast}");
         }
-        public void SetSchet ()
+        public void SetSchet()
         {
             Console.WriteLine($"Сейчас на счету: {_schet}");
             Console.WriteLine(" 1. Снять деньги \n 2. Положить деньги");
@@ -65,10 +66,41 @@ namespace Labs216.Shakirov
                     }
                     else
                     {
-                        Console.WriteLine($"На вашем счете: {_schet")
+                        Console.WriteLine($"На вашем счете: {_schet} money");
                     }
+                    break;
+                case "2":
+                    Console.WriteLine("Минимально 100");
+                    double put = double.Parse(Console.ReadLine());
+                    if (put < 100)
+                    {
+                        Console.WriteLine("Нет!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"На вашем счете: { _schet += put } money");
+                    }
+                    break;
+
             }
         }
+        public void SetStavka()
+        {
+            _stavka = 7.2;
+            Console.WriteLine($" Stavka = {_stavka} ");
+            Console.WriteLine(" Через сколько лет вы снимите деньги? ");
+            double let = double.Parse(Console.ReadLine());
+            double c;
+            for (int i = 0; i < let; i++)
+            {
+                _schet += _schet / 100 * _stavka;
+            }
+            Console.WriteLine($"Через {let} лет, ваш аккаунт будет иметь {_schet} денег ");
+        }
+        public void SetId(int NewSetId)
+        {
+
         }
     }
 }
+
