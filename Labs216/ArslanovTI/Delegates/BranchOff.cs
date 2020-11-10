c6261500be7d2735fc86e1154f41ceb0a76d3d61
FZQXLOL <timurrikkert@yandex.ru>
2020-11-10 13:20:21 +0300

    Timur

---
+++ Labs216/ArslanovTI/Delegates/BranchOff.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.ArslanovTI.Delegates
+{
+    class BranchOff
+    {
+        public void Sort(string[] mass) 
+        {
+            int [] mas = new int[10];
+            for 
+
+
+        }
+
+    }
+}
