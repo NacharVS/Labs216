250ea5f94e11c9fe6991fd1938aa15dd6bdbd4b9
ZU216 <garipovazuhra4@email.com>
2020-11-10 10:12:47 +0300

    minor fix

--- Labs216/Garipova/del/BranchOffice.cs
+++ Labs216/Garipova/del/BranchOffice.cs
@@ -1,11 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace Labs216.Garipova.del
 {
     class BranchOffice
     {
-        
+        public static void Corterovka()
+        {
+            int[] Array = new int[10];
+            Random rnd = new Random();
+   
+            for (int x = 0; x > Array.Length; x++)
+            {
+                Array[x] = rnd.Next(20, 50);      
+            }
+
+        }
+        public static void Summa()
+        {
+            int[] Array = new int[8];
+            Random rnd = new Random();
+
+            for (int x = 0; x > Array.Length; x++)
+            {
+                Array[x] = rnd.Next(10, 40);
+                Console.WriteLine("{Array[x]}");
+            }
+            int summ = 0;
+            summ += Array[x];
+            Console.WriteLine("{summ}");
+        }
+        public static void Maxx()
+        {
+            int[] Array = new int[8];
+            Random rnd = new Random();
+
+            for (int x = 0; x > Array.Length; x++)
+            {
+                Array[x] = rnd.Next(10, 40);
+                Console.WriteLine("{Array[x]}");
+            }
+             
     }
 }
--- Labs216/Program.cs
+++ Labs216/Program.cs
@@ -7,14 +7,7 @@ namespace Labs216
     {
         static void Main(string[] args)
         {
-            int[] Array = new int[10];
-            Random rnd = new Random();
-
-            for (int x = 0; x > Array.Length; x++)
-            {
-                Array[x] = rnd.Next(50);
-                Console.WriteLine(+ x + " = "+ Array[x]);
-            }
+            
 
         }
     }
