90f3542216bce2f401992d2a54b42ba874be870e
shay407 <world.deep@mail.ru>
2020-11-10 16:42:02 +0300

    done btanch

---
+++ Labs216/Shakirov/Delegates/Branch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Shakirov.Delegates
+{
+    class Branch
+    {
+        private delegate int[] ArrayOperation(int[] array);
+
+        public static void Main(string[] args)
+        {
+            ArrayOperation operation = GeneralOffice.Sorting;
+            operation += GeneralOffice.SummMassive;
+            operation += GeneralOffice.max;
+
+            Random rnd = new Random();
+            int[] array = new int[rnd.Next(5, 10)];
+            for (int i = 0; i < array.Length -1; i++)
+            {
+                array[i] = rnd.Next(1, 100);
+            }
+
+            Console.WriteLine("array");
+
+            for (int i=0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{array[i]}");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            operation();
+        }
+    }
+}
