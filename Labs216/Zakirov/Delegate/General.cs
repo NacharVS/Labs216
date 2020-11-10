2d866b246b290289f1fd71b2edc49cc3abc55dd7
Robert-ctr <z4kirov.robert@yandex.ru>
2020-11-10 13:12:05 +0300

    zabeginil

---
+++ Labs216/Zakirov/Delegate/General.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Zakirov.Delegate
+{
+    class General
+    {
+
+
+        public static void Corterovka()
+        {
+
+
+        }
+        public static void Summa()
+        {
+
+        }
+        public static void Maxx()
+        {
+
+        }
+    }
+
+}
+
