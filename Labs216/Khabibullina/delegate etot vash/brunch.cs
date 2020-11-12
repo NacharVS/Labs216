aaee29f0cb1483c7f446e1d2ed4dd4bb5729fbd4
Khabibullina216 <rinnamarin@gmail.com>
2020-11-12 15:01:32 +0300

    ya ne ponimayu kak norm delat commit
    p.s ponyala prinyala, sorry chto s opozdaniem

---
+++ Labs216/Khabibullina/delegate etot vash/brunch.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Khabibullina.delegate_etot_vash
+{
+    class brunch
+    {
+        static public void sum()
+        {
+            int[] array = new int[5];
+            Random rnd = new Random();
+            int x = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(100, 200);
+                Console.WriteLine($"{array[i]}");
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                x += array[i];
+            }
+            Console.WriteLine($"Summ: {x}");
+        }
+        static public void sort()
+        {
+            int[] array = new int[3];
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(1, 100);
+            }
+            Array.Sort(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i] + " ");
+            }
+        }
+        static public void max()
+        {
+            int[] array = new int[7];
+            Random rnd = new Random();
+            int y = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(100, 200);
+                Console.WriteLine($"{array[i]}");
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (y < array[i])
+                {
+                    y = array[i];
+                }
+            }
+            Console.WriteLine($" max: {y}");
+        }
+    }
+}
---
+++ Labs216/Khabibullina/delegate etot vash/general.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Khabibullina.delegate_etot_vash
+{
+    class general
+    {
+        private delegate void Mass();
+        public static void mass()
+        {
+            Mass mass = null;
+            mass += brunch.max;
+            mass += brunch.sort;
+            mass += brunch.sum;
+            mass();
+        }
+    }
+}
+
+
