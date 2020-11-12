b594ab9720e82b7a28365cf3d9ad54a730a1b9aa
shay407 <world.deep@mail.ru>
2020-11-12 14:40:57 +0300

    end branch

--- Labs216/Shakirov/Delegates/Branch.cs
+++ Labs216/Shakirov/Delegates/Branch.cs
@@ -4,33 +4,24 @@ using System.Text;
 
 namespace Labs216.Shakirov.Delegates
 {
-    class Branch
+    class BranchOffice
     {
-        private delegate int[] ArrayOperation(int[] array);
-
-        public static void Main(string[] args)
+        public static void MaxValue()
         {
-            ArrayOperation operation = GeneralOffice.Sorting;
-            operation += GeneralOffice.SummMassive;
-            operation += GeneralOffice.max;
-
             Random rnd = new Random();
-            int[] array = new int[rnd.Next(5, 10)];
-            for (int i = 0; i < array.Length -1; i++)
+            int[] array = new int[23];
+            int max = 0;
+            for (int i = 0; i < array.Length; i++)
             {
-                array[i] = rnd.Next(1, 100);
-            }
-
-            Console.WriteLine("array");
-
-            for (int i=0; i < array.Length; i++)
-            {
-                Console.WriteLine($"{array[i]}");
+                array[i] = rnd.Next(80);
+                Console.Write($" {array[i]}");
+                if (max < array[i])
+                    max = array[i];
             }
             Console.WriteLine();
-            Console.WriteLine();
-
-            operation();
+            Console.Write($"max: {max}");
+        }
         }
     }
-}
+
+
