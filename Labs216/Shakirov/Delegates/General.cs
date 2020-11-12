718348650fabda1b604f18fbd72f11bb9c80311d
shay407 <world.deep@mail.ru>
2020-11-12 14:54:46 +0300

    General end

--- Labs216/Shakirov/Delegates/General.cs
+++ Labs216/Shakirov/Delegates/General.cs
@@ -6,6 +6,19 @@ namespace Labs216.Shakirov.Delegates
 {
     class GeneralOffice
     {
-
+        public static void Sorting()
+        {
+            Random rnd = new Random();
+            int[] array = new int[17];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(10, 99);
+            }
+            Array.Sort(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($" {array[i]}");
+            }
+        }
     }
 }
