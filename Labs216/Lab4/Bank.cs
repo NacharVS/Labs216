9b6c02bbc403d8cc21b389ea00394e36fe5f7032
Azaliya <amalkaazaliya@gmail.com>
2020-11-17 22:26:57 +0300

    happy new year

--- Labs216/Lab4/Bank.cs
+++ Labs216/Lab4/Bank.cs
@@ -7,12 +7,17 @@ namespace Labs216.Lab4
 {
     class Bank
     {
-        public delegate void Friend(string phonenumber);
+        public delegate void Friend(int sum, string phonenumber);
+        public delegate void Texture(double stavka);
 
         private int _sum;
         private string _phoneNumber;
-        private static double _stavka = 6.7;
-        private double _score;
+        private static double _stavka; 
+        public Bank(double stavka)
+        {
+            _stavka = stavka;
+        }
+        
         public Bank(int sum, string phoneNumber)
         {
             _sum = sum;
@@ -27,7 +32,7 @@ namespace Labs216.Lab4
             private set
             {
                 _sum = value;
-                Notify?.Invoke(_phoneNumber);
+                Notify?.Invoke(_sum, _phoneNumber);
             }
         }
         public event Friend Notify;
@@ -42,26 +47,37 @@ namespace Labs216.Lab4
                 _phoneNumber = value;
             }
         }
-        public static void Bank1()
+        public event Texture Notify2;
+        public double Stavka
         {
-            
-        }
-        public static void SetStavka()
-        {
-            double _score = 17000;
-            double _stavka = 6.7;
-            Console.WriteLine($"Stavka in our bank - {_stavka}%");
-            Console.WriteLine($"our score {_score}");
-            Console.WriteLine("How many years would you like to withdraw money?");
-            double years = double.Parse(Console.ReadLine());
-            double i = 0;
-            for ( i = 0; i < years; i++)
+            get
             {
-                i = ((100 * _score) / _stavka) * 100;
+                return _stavka;
             }
-            Console.WriteLine($"Your account will have {i} in {years} years");
+            private set
+            {
+                var oldStavka = _stavka;
+                _stavka = value;
+                Notify2?.Invoke(_stavka);
+            }
         }
-        public void StavkaChange(double newStavka)
+        public void Deposit(int sum)
+        {
+            Sum += sum;
+        }
+        public void Withdraw(int sum)                   
+        {
+            Sum -= sum;
+        }
+        public void StavkaChange(double stavka)
+        {
+            stavka = (stavka * Sum) / 100 ;
+        }
+        public static void DateTimeStavka()
+        {
+
+        }
+        public static void Cashbaсk()
         {
 
         }
--- Labs216/Program.cs
+++ Labs216/Program.cs
@@ -13,6 +13,22 @@ namespace Labs216
         static void Main(string[] args)
         {
             Bank account = new Bank(999, "+79872314564");
+            
+            account.Deposit(100);
+            account.Withdraw(99);
+            account.Deposit(500);
+        }
+        public static void Message(int sum, string phoneNumber)
+        {
+            Console.WriteLine($"Message send at {phoneNumber}");
+        }
+        public static void Message2(int sum, string phoneNumber)
+        {
+            Console.WriteLine($"Account value {sum}");
+        }
+        public static void Message3(double stavka)
+        {
+            Console.WriteLine($"");
         }
     }
 }
