04b452900086455379d6514f5714fe810bb6f0ef
Robert-ctr <z4kirov.robert@yandex.ru>
2020-11-18 15:09:27 +0300

    peredelal

--- Labs216/Zakirov/Delegate/General.cs
+++ Labs216/Zakirov/Delegate/General.cs
@@ -4,44 +4,123 @@ using System.Text;
 
 namespace Labs216.Zakirov.Delegate
 {
-    class General
+    class Bank
     {
-
-        public static void Sorting(int[] mass)
+        private string _name;
+        private string _surname;
+        private string _phoneNumber;
+        private string _id;
+        private static double _1xBet; //Ставка
+        private double _Cash;
+        private static int _count = 1;
+        private static int _Max = 100000;
+        private static int _Min = 1000;
+        private int _age;
+        
+        public string Name
         {
-            Array.Sort(mass);
-            Console.Write("Sorting:");
-            for (int a = 0; a < mass.Length; a++)
+            get { return _name; }
+            private set
             {
-                Console.Write($" {mass[a]}");
-
+                if (value != "")
+                {
+                    value = value.Trim();
+                    _name = value[0].ToString().ToUpper() + value.Substring(1);
+                }
+                else _name = "defualt";
+            }
+        }
+        public string Surname
+        {
+            get { return _surname; }
+            private set
+            {
+                if (value != "")
+                {
+                    value = value.Trim();
+                    _surname = value[0].ToString().ToUpper() + value.Substring(1);
+                }
+                else _surname = "defualt";
+            }
+        }
+        public int Age
+        {
+            get { return _age; }
+            private set
+            {                
+               _age = value;            
+            }
+        }
+        public string Id
+        {
+            get { return _id; }
+        }
+        public double Account
+        {
+            get { return _Cash; }
+            private set
+            {
+                _Cash = value;
+                //Notify?.Invoke(_phoneNumber, $"Acount Change {_account}");
+            }
+        }
+        public static int Count
+        {
+            get { return _count; }
+        }
+        private void GetId()
+        {
+            _id = _name + _surname + "_" + _count;
+            _count = _count + 1;
+        }
+        private void GetAge()
+        {
+            Console.WriteLine("Write you birthday (dd mm yyyy)");
+            string[] date = new string[3];
+            date = Console.ReadLine().Split(" ");
+            int day = int.Parse(date[0]);
+            int month = int.Parse(date[1]);
+            int year = DateTime.Now.Year - int.Parse(date[2]) - 1;
+            {
+                if (DateTime.Now.Month >= month & DateTime.Now.Day >= day) Age = year + 1;
+            }
+        }
+        public Bank()
+        {
+            Console.WriteLine("Write you name");
+            Name = Console.ReadLine();
+            Console.WriteLine("Write you surname");
+            Surname = Console.ReadLine();
+            Console.WriteLine("Write you phone");
+            _phoneNumber = Console.ReadLine();
+            GetAge(); //Вызвал метод 79
+            GetId(); //Вызвал метод 74
+        }
+        //Нужно сделать методы депозит, снять, ставка, и что-то ещё, чтобы всё работало.
+        public void Deposit(int Value)
+        {
+            if (Value >= _Min)
+            {
+                Account = Account + Value;
             }
 
         }
-         public static void SummArray(int[] mass)
+        public void Withdraw(int Value)
         {
-            int Summ = 0;
-            for (int a = 0; a < mass.Length; a++)
+            
+            if (Value <= _Max)
             {
-                if (mass[a] > 0)
-                    Summ += mass[a];
+                Account = Account - Value;
             }
-            Console.WriteLine();
-            Console.Write($"Summ: {Summ}");
         }
-        public static void MaxValue(int[] mass)
+        public void Mirror(int Year)
         {
-            int max = 0;
-            for (int a = 0; a < mass.Length; a++)
+            double buff = _Cash;
+            for (int i = 0; i < Year; i++)
             {
-                Console.Write($" {mass[a]}");
-                if (max < mass[a])
-                    max = mass[a];
+                buff = buff + buff * _1xBet / 100;
             }
-            Console.WriteLine();
-            Console.WriteLine($"max: {max}");
         }
-
 
     }
 }
