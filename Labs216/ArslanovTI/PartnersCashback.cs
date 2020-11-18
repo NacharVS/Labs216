6a9d8774e03602cd751bd5c6b5889e3cd1ac0401
FZQXLOL <timurrikkert@yandex.ru>
2020-11-18 14:36:06 +0300

    Сделал отдельный класс для партнеров PartnersCashback , там установил кешбеки для каждой из 3ёх компаний

---
+++ Labs216/ArslanovTI/PartnersCashback.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.ArslanovTI
+{
+    class PartnersCashback
+    {
+        public void McDonalds(double cashback, double sumpokupok, double sum)
+        {
+            cashback = 0.2;
+            Console.WriteLine("Введите сумму покупок из чека");
+            sumpokupok = int.Parse(Console.ReadLine());
+            sum += sumpokupok * cashback;
+
+            Console.WriteLine($"На ваш счет было зачислено", sumpokupok * cashback, " Ваш баланс составляет", sum);
+        }
+
+
+
+        public void Amazon(double cashback, double sumpokupok, double sum)
+        {
+            cashback = 0.1;
+            Console.WriteLine("Введите сумму покупок из чека");
+            sumpokupok = int.Parse(Console.ReadLine());
+            sum += sumpokupok * cashback;
+
+            Console.WriteLine($"На ваш счет было зачислено", sumpokupok * cashback, " Ваш баланс составляет", sum);
+        }
+
+
+        public void Apple(double cashback, double sumpokupok, double sum)
+        {
+            cashback = 0.24;
+            Console.WriteLine("Введите сумму покупок из чека");
+            sumpokupok = int.Parse(Console.ReadLine());
+            sum += sumpokupok * cashback;
+
+            Console.WriteLine($"На ваш счет было зачислено", sumpokupok * cashback, " Ваш баланс составляет", sum);
+
+        }
+    }
+}
