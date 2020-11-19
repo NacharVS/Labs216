b6e06d2bf603087d053f0e3cedf78c21c9215dd5
Dimukhametov <ilnazdima@mail.ru>
2020-11-19 15:04:29 +0300

    Делал на паре

---
+++ Labs216/Dimukhametov/Strategy/Soldiers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Dimukhametov.Strategy
+{
+    class Soldiers
+    {
+        class knihgts
+        {
+            public string TypeOffAttack; // millitary or renght
+            public int HeatPoints = 100;
+            public int damage = 50;
+        }
+        class archers
+        {
+            public string TypeOffAttack; // millitary or renght
+            public int HeatPoints = 75;
+            public int damage = 25;
+        }
+        class magicians
+        {
+            public string TypeOffAttack; // millitary or renght
+            public int HeatPoints = 50;
+            public int damage = 30;
+        }
+        class catapults
+        {
+            public string TypeOffAttack; // millitary or renght
+            public int HeatPoints = 200;
+            public int damage = 100;
+        }
+        class scouts
+        {
+            public string TypeJffAttack; // millitary or renght
+            public int HeatPoints = 25;
+            public int damage = 10;
+        }
+    }
+}
