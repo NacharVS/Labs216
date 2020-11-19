4f9639f37a82005ce12eeb8cbfb7a9f3e3116c1c
bu1at <gabdulkhaev03@mail.ru>
2020-11-19 15:09:39 +0300

    STRATEGIYA

---
+++ Labs216/Gabdulkhaev/Стратгия/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Gabdulkhaev.Стратгия
+{
+    interface Personazh
+    {
+        string name { get; set; }
+        double age { get; set; }
+        double gender { get; set; } // human or woman
+        double health_point {get; set;}
+        double damag { get; set; }
+        double protection { get; set; }
+        double luck { get; set; }
+        double speed { get; set; }
+    }
+    public class ASRFAJSFJAJSF:Personazh
+    {
+        public string name { get; set; }
+        public double age { get; set; }
+        public double gender { get; set; } // human or woman
+        public double health_point { get; set; }
+        public double damag { get; set; }
+        public double protection { get; set; }
+        public double luck { get; set; }
+        public double speed { get; set; }
+    }
+//    public void Attack()
+  //  {
+    // }
+}
