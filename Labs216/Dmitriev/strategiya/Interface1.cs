720cdae46a5832302ede3516cc3ed768c276cf13
DSeregaV <ddmmiittrriieevv2003@gmail.com>
2020-11-19 15:10:37 +0300

    begin

---
+++ Labs216/Dmitriev/strategiya/Interface1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Dmitriev.strategiya
+{
+    interface Interface1
+    {
+        const int minSpeed = 0;
+        static int maxSpeed = 20;
+        void Move();
+        string Name { get; set; }
+
+        delegate void MoveHandler(string message);
+        event MoveHandler MoveEvent;
+    }
+}
