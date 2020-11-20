5b05931684e716663e90c27fd3348f81aba2313b
AlexanderTarskih <jiumohad2017@yandex.ru>
2020-11-20 12:15:46 +0300

    Units Part I

---
+++ Labs216/Tarskih/Strategy/Units.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Tarskih.Strategy
+    {
+        class Units
+        {
+            public abstract class MovableObject
+            {
+                public abstract void DefineCover();
+                public abstract void Move(int dx, int dy);
+                public abstract bool MoveNode(int i, int dx, int dy);
+            }
+            public interface iVillager
+            {
+                int GetVillager(int health = 100, int armor = 0, int craft = 1, int medication = 1)
+                    int Stats { get; set; }
+            }
+        }
+    }
+}
+}
