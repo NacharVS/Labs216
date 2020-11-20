993f9c7e076118327349f725c06de1fd77b471b9
Robert-ctr <z4kirov.robert@yandex.ru>
2020-11-20 13:17:02 +0300

    chtoto sdelal

--- Labs216/Zakirov/Strategygame/Army.cs
+++ Labs216/Zakirov/Strategygame/Army.cs
@@ -4,8 +4,7 @@ using System.Text;
 
 namespace Labs216.Zakirov.Strategygame
 {
-    class Game
+    class Army
     {
-       
     }
 }
---
+++ Labs216/Zakirov/Strategygame/Interface.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Zakirov.Strategygame
+{
+    interface IMovable
+    {
+        public double Speed { get; }
+        public string UnitType { get; }
+        public void Move();
+
+    }
+    interface IShootable
+    {
+        public int ShootDamage { get; }
+        public int Distance { get; }
+        public void Shoot();
+
+    }
+    interface IWorkers : IMovable
+    {
+        public void Work();
+    }
+    interface IBuilding
+    {
+        public void Interact();
+    }
+    interface IArmy : IMovable
+    {
+        public int Damage { get; }
+        public void Attack();
+    }
+
+
+    
+}
