7e167c86a087f4c1bdf9497ceb2918cccf4ba226
DSeregaV <ddmmiittrriieevv2003@gmail.com>
2020-11-20 13:15:47 +0300

    begin

---
+++ Labs216/Dmitriev/strategiya/infa1.cs
@@ -0,0 +1,29 @@
+﻿namespace Labs216.Dmitriev.strategiya
+{
+    internal class infa4elovek
+    {
+        public stena(int zdorove, int oborona, )
+        {
+            Zdorove = zdorove;
+
+            Oborona = oborona;
+        }
+
+        public void Interact()
+        {
+            Console.WriteLine($" это стена что бы ее  преодолеть  {Zdorove} жизнь");
+        }
+        public bshnya(int zdorove, int oborona, )
+        {
+            Zdorove = zdorove;
+
+            Oborona = oborona;
+        }
+
+        public void Interact()
+        {
+            Console.WriteLine($" преодолеть препятствие  {Zdorove} жизнь");
+        }
+    }
+}
+
