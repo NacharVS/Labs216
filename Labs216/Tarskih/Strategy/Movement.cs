9fcad5d467251a981a6edb0c67f06dc647d543a2
AlexanderTarskih <jiumohad2017@yandex.ru>
2020-11-20 12:30:54 +0300

    Strategy

---
+++ Labs216/Tarskih/Strategy/Movement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Tarskih.Strategy
+{
+    class Movement
+    {
+        public abstract class MovableObject
+        {
+            public abstract void Move(int dx, int dy);
+            public abstract bool MoveNode(int i, int dx, int dy);
+        }
+    }
+}
