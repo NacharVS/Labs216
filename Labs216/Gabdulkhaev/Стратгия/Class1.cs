fea301aac04b4f2d5e8621e15a03e6e5d20239a7
bu1at <gabdulkhaev03@mail.ru>
2020-11-20 13:17:46 +0300

    интерфасе на паре

--- Labs216/Gabdulkhaev/Стратгия/Class1.cs
+++ Labs216/Gabdulkhaev/Стратгия/Class1.cs
@@ -4,29 +4,102 @@ using System.Text;
 
 namespace Labs216.Gabdulkhaev.Стратгия
 {
-    interface Personazh
+    public interface Personazh
     {
         string name { get; set; }
         double age { get; set; }
-        double gender { get; set; } // human or woman
-        double health_point {get; set;}
+        bool gender { get; set; } // human(true) or woman(fALSE)
+        double health_point { get; set; }
         double damag { get; set; }
+        int bronya { get; set; }
         double protection { get; set; }
-        double luck { get; set; }
         double speed { get; set; }
     }
-    public class ASRFAJSFJAJSF:Personazh
+    class prikolist : Personazh
     {
-        public string name { get; set; }
-        public double age { get; set; }
-        public double gender { get; set; } // human or woman
-        public double health_point { get; set; }
-        public double damag { get; set; }
-        public double protection { get; set; }
-        public double luck { get; set; }
-        public double speed { get; set; }
+
+        public string name
+        {
+            get
+            {
+                name = Console.ReadLine();
+            }
+        }
+        public double age
+        {
+            set
+            {
+                int sozdanie = DateTime.Now.Millisecond;
+                int tekuch = Convert.ToInt32(DateTime.Now - sozdanie);
+                age = tekuch;
+            }
+        }
+        public bool gender { get; set; }
+        public double health_point
+        {
+            set
+            {
+                double health_point = 100;
+            }
+        }
+        public double damag
+        {
+            set
+            {
+                double svoi = 10;
+                Random rnd = new Random();
+                double luck = rnd.Next(0, 20);
+                double damag = svoi + luck*speed;
+            }
+        }
     }
-//    public void Attack()
-  //  {
-    // }
+    public double protection
+    {
+        set
+        {
+            int vibor = Console.ReadLine();
+            switch (vibor)
+            {
+                case 1:
+                    Console.WriteLine("Легкая броня");
+                    protection = 50;
+                    break;
+                case 2:
+                    Console.WriteLine("Средняя броня");
+                    protection = 100;
+                    break;
+                case 3:
+                    Console.WriteLine("Тяжелая броня");
+                    protection = 150;
+                    break;
+            }
+        }
+    }
+    public double speed
+    {
+        set
+        {
+            Random rnd = new Random();
+            int randomspeed = rnd.Next(1, 3);
+            switch (randomspeed)
+            {
+                case 1:
+                    Console.WriteLine("Не повезло");
+                    speed = 0.5;
+                    break;
+                case 2:
+                    Console.WriteLine("+-");
+                    speed = 1;
+                    break;
+                case 3:
+                    Console.WriteLine("оп");
+                    speed=1.5;
+                    break;
+            }
+        }
+    }
+    public void Attack()
+    {
+        throw new NotImplementedException();
+    }
 }
