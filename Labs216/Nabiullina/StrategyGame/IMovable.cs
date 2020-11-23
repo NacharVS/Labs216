e221d5a7d1e4cb365ea5cc9a18403f1e2fcc1ae0
Zulfia <zulfianabiullina4998@.com>
2020-11-23 13:08:43 +0300

    interface movable

---
+++ Labs216/Nabiullina/StrategyGame/IMovable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Nabiullina.StrategyGame
+{
+    interface IMovable
+    {
+        public const int minSpeed = 0;
+        private static int maxSpeed = 60;
+        static double GetTime(double distance, double speed) => distance / speed;
+        static int MaxSpeed
+        {
+            get { return maxSpeed; }
+            set
+            {
+                if (value > 0) maxSpeed = value;
+
+            }
+        }
+        public void Move()
+        {
+            Console.WriteLine("Walking");
+        }
+        protected internal string Name { get; set; }
+        public delegate void MoveHandler(string massage);
+        public event MoveHandler MoveEvent;
+    }
+}
