a66bb738c6ee55962cdb3c8fc5f1b7cb9d6f881f
Zulfia <zulfianabiullina4998@.com>
2020-11-24 23:27:15 +0300

    interfacceeeeeee '_'

---
+++ Labs216/Nabiullina/StrategyGame/INTERFACE.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Nabiullina.StrategyGame
+{
+    interface IShoot
+    {
+        //srtelyat
+        public void Shoot();
+    }
+    interface IMovement
+    {
+        //peredvigenie
+        public void Movement();
+    }
+    interface ICharge
+    {
+        //zaryad, ataka
+        public void Charge();
+    }
+    interface IRevive
+    {
+        //ozhivat'
+        public void Revive();
+    }
+    interface ITreat
+    {
+        //lechit'
+        public void Treat();
+    }
+    interface IIncome
+    {
+        //pribbIl'
+        public void Income();
+    }
+
+    interface IWork
+    {
+        //pabota
+        public void Work();
+    }
+
+
+}
+
--- Labs216/Nabiullina/StrategyGame/Mirn jiteli/Doctor.cs
+++ Labs216/Nabiullina/StrategyGame/Mirn jiteli/Doctor.cs
@@ -4,7 +4,8 @@ using System.Text;
 
 namespace Labs216.Nabiullina.StrategyGame
 {
-    class Doctor
+    class Doctor:ITreat
     {
+       
     }
 }
