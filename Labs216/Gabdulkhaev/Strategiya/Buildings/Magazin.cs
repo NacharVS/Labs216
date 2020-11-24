ad4eab320912a1635f8347978f167d21fdf9940f
bu1at <gabdulkhaev03@mail.ru>
2020-11-24 13:20:18 +0300

    vse

---
+++ Labs216/Gabdulkhaev/Strategiya/Buildings/Magazin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Gabdulkhaev.Strategiya.Buildings
+{
+    class Magazin:IRabotable
+    {
+        public void Rabota()
+        {
+            Console.WriteLine("Магазин"); // ну получается шахтер приносит деньги с шахты, медик платно лечит, кузнец платно чинит, бойцы грабят, деньги есть, фермер не нужен
+            // по идее бойцы могут тупо ограбить магазин, но нет
+            Console.WriteLine("Еда(1)");
+            Console.WriteLine("Не еда(2)");
+            int VIbor = Convert.ToInt32(Console.ReadLine());
+            switch (VIbor)
+            {
+                case 1:
+                    Console.WriteLine("Вы выбрали раздел с едой");
+                    Console.WriteLine("Выберите еду");
+                    Console.WriteLine("Еда1 под номером1");
+                    Console.WriteLine("Еда2 под номером2");
+                    Console.WriteLine("Еда3 под номером3");
+                    Console.WriteLine("Еда4 под номером4");
+                    Console.WriteLine("Еда5 под номером5");
+                    int eda = Convert.ToInt32(Console.ReadLine());
+                    switch (eda)
+                    {
+                        case 1:
+                            Console.WriteLine("Сколько?");
+                            break;
+                        case 2:
+                            Console.WriteLine("Сколько?");
+                            break;
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Вы выбрали не еду");
+                    break;
+            }
+            double biudget = 0; // надо бюджет сделать какой-то
+        }
+    }
+}
--- Labs216/Gabdulkhaev/Strategiya/Peaceful people/Medik.cs
+++ Labs216/Gabdulkhaev/Strategiya/Peaceful people/Medik.cs
@@ -5,11 +5,23 @@ using Labs216.Gabdulkhaev.Strategiya.Boets;
 
 namespace Labs216.Gabdulkhaev.Strategiya.Peaceful_people
 {
-    class Medik:Character,ILechitable,IHoditable, INanositUronable
+    class Medik:Character,IHoditable, INanositUronable, IRabotable
     {
-        public void Lechit()
+        public void Rabota()
         {
-            Console.WriteLine("Медик лечит бойцов");
+            int medicinskie_prikoli = 1000; // медикаменты
+            Random rnd = new Random();
+            int medik_Lech = rnd.Next(1, 3); // может вылечит, а может не знает как лечить
+            Random rnd1 = new Random();
+            int medik_lech1 = rnd.Next(1, 10); // вылечит на несколько хп (до 10)
+            if (medik_Lech == 1&&medicinskie_prikoli>0)
+            {
+                Console.WriteLine($"Медик вылечил бойца на +{medik_lech1} hp");
+            }
+            else if (medik_Lech == 2 ||medicinskie_prikoli<=0)
+            {
+                Console.WriteLine("Медик не знает как лечить или у него нет медикаментов");
+            }
         }
         public void Hodit()
         {
