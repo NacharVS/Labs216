4a9d1ef2bffe109d39ea47c50ed2239fe599f0d3
DSeregaV <ddmmiittrriieevv2003@gmail.com>
2020-11-24 13:18:28 +0300

    v processe

--- Labs216/Dmitriev/strategiya/waru.cs
+++ Labs216/Dmitriev/strategiya/waru.cs
@@ -63,8 +63,65 @@ namespace Labs216.Dmitriev.strategiya
             {
                 Console.WriteLine($" направление  {UnitType}  скорость: {Speed}");
             }
-        }
     }
+    
+        {
+            public int Damage { get; private set; }
+            public double Speed { get; private set; }
+            public string UnitType { get; } = "water";
+            public int Distance { get; private set; }
+            public int ShootDamage { get; private set; }
 
-}
+            public lodka(int zdorove, int oborona, double speed, int uron)
+            {
+                Zdorove = zdorove;
+                Defense = oborona;
+                Speed = speed;
+                Damage = uron;
+            }
+
+                public void Attack()
+                {
+                    Console.WriteLine($"урон: {Damage}");
+                }
+
+                public void Move()
+                {
+                    Console.WriteLine($" {UnitType}скорость : {Speed}");
+                }
+
+                public void Shoot()
+                {
+                    Console.WriteLine($": {ShootDamage}");
+                }
+            }
+
+            sealed class samolet : Infa, IMovable, IShootable
+            {
+                public int Damage { get; private set; }
+                public double Speed { get; private set; }
+                public string UnitType { get; } = "воздух";
+                public int Distance { get; private set; }
+                public int ShootDamage { get; private set; }
+
+                public samolet(int zdorove, int oborona, double speed, int uron)
+                {
+                    Health = zdorove;
+                    Defense = oborona;
+                    Speed = speed;
+                    Damage = uron;
+                }
+
+                public void Attack()
+                {
+                    Console.WriteLine($"урон: {Damage}");
+                }
+
+                public void Move()
+                {
+                    Console.WriteLine($"двигатся  {UnitType} скорость: {Speed}");
+                }
+
+            } 
+
 
