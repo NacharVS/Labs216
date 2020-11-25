5c6b147b2acf93caac59d152b83037988f93dd08
islam7898 <is.fs.ilsh@gmail.com>
2020-11-25 14:42:10 +0300

    Stratagy

--- Labs216/Faskhutdinov/interface play/mirnyi/pojarny.cs
+++ Labs216/Faskhutdinov/interface play/mirnyi/pojarny.cs
@@ -4,7 +4,29 @@ using System.Text;
 
 namespace Labs216.Faskhutdinov.interface_play.mirnyi
 {
-    class pojarny
+    class pojarny : Characteristic, IMove, IAtac, IMine
+
     {
+        public int domak { get; }
+
+        int IMine.mine => throw new NotImplementedException();
+
+        public int mine;
+
+        public void Atac()
+        {
+
+        }
+
+        public void Mine()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move()
+        {
+
+        }
+
     }
-}
+}  
