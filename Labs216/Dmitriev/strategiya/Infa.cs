c30c8c040da3ebae2a6df7758ea554bf83fcf336
DSeregaV <ddmmiittrriieevv2003@gmail.com>
2020-11-27 13:01:07 +0300

    1

--- Labs216/Dmitriev/strategiya/Infa.cs
+++ Labs216/Dmitriev/strategiya/Infa.cs
@@ -4,24 +4,20 @@ using System.Text;
 
 namespace Labs216.Dmitriev.strategiya
 {
-    class Infa
-    {
         public abstract class infa4ela
         {
-            public int _health { get; set; }
-            public int _defense { get; set; }
-            public int _DMG { get; set; }
+            public int _Zdorove { get; set; }
+            public int _Oborona { get; set; }
+            public int _uron { get; set; }
             public int _speed { get; set; }
         }
 
         public abstract class infaBuilding
         {
-            public int _health { get; set; }
-            public int _defense { get; set; }
-            public int _dmgBuild { get; set; }
-            public bool IsDestroyed { get; set; }
+            public int _Zdorove { get; set; }
+            public int _Oborona { get; set; }
+            public int _uron { get; set; }
+            public bool Razrushen { get; set; }
         }
     }
 
-}
-}
