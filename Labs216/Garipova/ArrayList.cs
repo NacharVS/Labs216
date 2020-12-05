1134882cd8dbf31bd9ea0a0eb4db84595b6c44b2
ZU216 <garipovazuhra4@email.com>
2020-12-05 13:14:04 +0300

    minor fix

---
+++ Labs216/Garipova/ArrayList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Labs216.Garipova
+{
+    class ArrayList
+    {
+        internal void Add(int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Add(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Add(double v)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
