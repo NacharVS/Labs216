745ebb5fcadb807ffc1724529f0dabce67123dd0
bu1at <gabdulkhaev03@mail.ru>
2020-12-05 12:59:13 +0300

    sort

---
+++ Labs216/Gabdulkhaev/Sortlist.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+namespace Labs216.Gabdulkhaev
+{
+    class Sortlist
+    {
+        public static void array()
+        {
+            ArrayList LList = new ArrayList();
+            LList.Add("оп");
+            LList.Add(2.3);
+            LList.Add(55);
+            LList.Add("фыддф");
+            LList.Add(5.084);
+            LList.Add(243);
+            LList.Add("фысьфысь");
+            LList.Add(0.0001);
+            LList.Add(1);
+            LList.Add("строка");
+            LList.Add(100.001);
+            LList.Add(1000);
+            // отсортировать на int, double and string и раскидать по трем разным массивам ( либо коллекциям)
+        }
+}
+}
