7f8b93127504d6e53098a3501874caca8dbd359e
AnisimovIvan <161901@mck-ktits.ru>
2020-12-05 12:54:51 +0300

    SortArray

---
+++ Labs216/Anisimov/ArrayList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Anisimov
+{
+    class PracticArrayList
+    {
+        public static void ListSort()
+        {
+            ArrayList List = new ArrayList() { 1, "11", 1.1, 2, 3, 4, 5, "22", "33", "44", "55", 2.2, 3.3, 4.4, 5.5 };
+
+            ArrayList IntList = new ArrayList();
+            ArrayList StringList = new ArrayList();
+            ArrayList DoubleList = new ArrayList();
+
+            foreach(var item in List)
+            {
+                switch (item.GetType().ToString())
+                {
+                    case "System.Int32":
+                        IntList.Add(item);
+                        break;
+                    case "System.String":
+                        StringList.Add(item);
+                        break;
+                    case "System.Double":
+                        DoubleList.Add(item);
+                        break;
+                }
+            }
+
+            Console.WriteLine("int");
+            Console.WriteLine();
+            
+            foreach(var item in IntList)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("string");
+            Console.WriteLine();
+
+            foreach (var item in StringList)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("double");
+            Console.WriteLine();
+
+            foreach (var item in DoubleList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
--- Labs216/Program.cs
+++ Labs216/Program.cs
@@ -10,7 +10,7 @@ namespace Labs216
     {
         static void Main(string[] args)
         {
-            RunBank.RunBank2();
+            PracticArrayList.ListSort();
         }
     }
 }
