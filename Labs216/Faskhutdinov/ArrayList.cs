234331fd612d9b5f880724278abc7d6c687d0611
islam7898 <is.fs.ilsh@gmail.com>
2020-12-08 12:29:09 +0300

    Araylisrrrr

---
+++ Labs216/Faskhutdinov/ArrayList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Faskhutdinov
+{
+    class ArrayList
+    {
+        public static void ListSert();
+       }
+          ArrayList List = new ArrayList() {
+}
