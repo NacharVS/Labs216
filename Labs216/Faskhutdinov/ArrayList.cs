fb5fbd83afea56cbbf3ffb78f27c8bab60349ac4
islam7898 <is.fs.ilsh@gmail.com>
2020-12-08 12:54:10 +0300

    ошибка выходит

--- Labs216/Faskhutdinov/ArrayList.cs
+++ Labs216/Faskhutdinov/ArrayList.cs
@@ -8,10 +8,10 @@ namespace Labs216.Faskhutdinov
     {
         public static void ListSert();
        }
-    ArrayList List = new ArrayList() { -23, "54", 1.7, "30", -24, 2.5, 25, 26, "12", "43", 4 , "21", 4.3, 3.4, 5.5 };
-    private ArrayList StringList = new ArrayList();
-    private ArrayList DoubleList = new ArrayList();
-    private ArrayList IntList = new ArrayList();
+       ArrayList List = new ArrayList() { -23, "54", 1.7, "30", -24, 2.5, 25, 26, "12", "43", 4 , "21", 4.3, 3.4, 5.5 };
+       private ArrayList StringList = new ArrayList();
+       private ArrayList DoubleList = new ArrayList();
+       private ArrayList IntList = new ArrayList();
 
 
 
