df960914dd919efe6f928d116bce49681b8877a3
DSeregaV <ddmmiittrriieevv2003@gmail.com>
2020-12-09 14:17:59 +0300

    klekciya

--- Labs216/Dmitriev/Bank_collection/kollekciya.cs
+++ Labs216/Dmitriev/Bank_collection/kollekciya.cs
@@ -4,7 +4,48 @@ using System.Text;
 
 namespace Labs216.Dmitriev.Bank_collection
 {
-    class kollekciya
+    abstract class Klient
     {
+        private string _name;
+        private string _surname;
+
+        public string Name
+        {
+            get => _name;
+            set => _name = $"{value[0].ToString().ToUpper() + value.Substring(1)}";
+        }
+
+        public string Surname
+        {
+            get => _surname;
+            set => _surname = $"{value[0].ToString().ToUpper() + value.Substring(1)}";
+        }
+
+        public virtual void GetInform()
+        {
+            Console.WriteLine($"li4nostb klienta: {Name}{Surname}");
+        }
+        abstract class Klient
+        {
+            private string _name;
+            private string _surname;
+
+            public string Name
+            {
+                get => _name;
+                set => _name = $"{value[0].ToString().ToUpper() + value.Substring(1)}";
+            }
+
+            public string Surname
+            {
+                get => _surname;
+                set => _surname = $"{value[0].ToString().ToUpper() + value.Substring(1)}";
+            }
+
+            public virtual void GetInform()
+            {
+                Console.WriteLine($"Your name: {Name}{Surname}");
+            }
+        }
     }
 }
