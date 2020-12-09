7104d5ab36180fa577a256f9203ab7e617ce2453
FZQXLOL <timurrikkert@yandex.ru>
2020-12-09 15:00:38 +0300

    para

---
+++ Labs216/ArrayList1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216
+{
+    class ArrayList1
+    {
+        public void ArrayL()
+
+        {
+            ArrayList array = new ArrayList();
+            ArrayList arrayOfInt = new ArrayList();
+            ArrayList arrayofstring = new ArrayList();
+            ArrayList arrayofDouble = new ArrayList();
+            array.Add("Timur");
+            array.Add(24);
+            array.Add(3.13);
+            array.Add("T");
+            array.Add(26);
+            array.Add(2.13);
+            array.Add("апвпа");
+            array.Add(26);
+            array.Add("lflff");
+            array.Add(6.15);
+
+
+            foreach (var item in array)
+            {
+                if (item.GetType() == typeof(int))
+                {
+                    arrayOfInt.Add(item);
+                }
+
+                if (item.GetType() == typeof(string))
+                {
+                    arrayofstring.Add(item);
+                }
+                if (item.GetType() == typeof(double))
+                {
+                    arrayofDouble.Add(item);
+                }
+            }
+
+
+
+
+        }
+    }
+}
--- Labs216/ArslanovTI/Bank.cs
+++ Labs216/ArslanovTI/Bank.cs
@@ -4,17 +4,39 @@ using System.Text;
 
 namespace Labs216.ArslanovTI
 {
+    class User
+    {
+        private string _name;
+        private string _surname;
+        private DateTime _DayOfBir;
+        private long _id;
+
+
+        public User(string name, string surname, DateTime DayOfBirt, long id)
+        {
+            _name = name;
+            _surname = surname;
+            _DayOfBir = DayOfBirt;
+            _id = id;
+            
+        }
+    }
     class Bank
     {
         private string _name;
         private string _surname;
-        private string _id;
+        private static long _id;
         private static double _rate;
         private double addMoney;
         private double withdrawMoney;
         double sum = 0;
         public string cashback;
-         
+        private long number;
+        public DateTime DayOfBirth;
+        int age;
+
+
+
 
         public void SetName(string newName)
         {
@@ -33,12 +55,14 @@ namespace Labs216.ArslanovTI
             var otherLetters = newsurName.Remove(0, 1);
             _surname = (firstletter.ToString().ToUpper() + otherLetters);
         }
-        public void SetId(string newsurName)
+        public void SetId(long newSetId)
         {
             Random rnd = new Random();
-            int id = rnd.Next(99999, 100000);
+            long id = rnd.Next(99999, 100000000);
+            id = _id;
+
         }
-        public void SetRate(string newsurName)
+        public void SetRate(string newSetRate)
         {
 
             double rate = 0.13;
@@ -61,36 +85,22 @@ namespace Labs216.ArslanovTI
             withdrawMoney = sum;
 
         }
+        void SetAge()
+        {
+            DateTi
+        }
 
         public delegate void Partners(string partn);
         public event Partners Notify;
 
-    //    public void McDonalds(double cashback1)
-    //    {
-    //        cashback1 = 0.1;
 
-    //        Notify?.Invoke($"На счет поступило{sum * 0.1} кэшбека");
 
-    //    }
-    //    public void Apple(double cashback2)
-    //    {
-    //        cashback2 = 0.3;
-    //        Notify?.Invoke($"На счет поступило{sum * 0.3} кэшбека");
-
-    //    }
-    //    public void Zara(double cashback3)
-    //    {
-    //        cashback3 = 0.3;
-    //        Notify?.Invoke($"На счет поступило{sum * 0.3} кэшбека");
-
-    //    }
-  
-    public string Cashback1
+        public string Cashback1
         {
             get { return cashback; }
             set { Notify?.Invoke($"На счет поступило{sum * 0.1} кэшбека"); }
         }
-             public string Cashback2
+        public string Cashback2
         {
             get { return cashback; }
             set { Notify?.Invoke($"На счет поступило{sum * 0.3}кэшбека"); }
@@ -100,6 +110,29 @@ namespace Labs216.ArslanovTI
             get { return cashback; }
             set { Notify?.Invoke($"На счет поступило{sum * 0.3}кэшбека"); }
         }
+
+        public void Users()
+        {
+
+            List<User> Users1 = new List<User>();
+
+            Users1.Add("Rudolf", "Nuriev", DateTimeKind(2005,5,8)) ;
+            Users1.Add("Ralph");
+            Users1.Add("Timur");
+            Users1.Add("Cher");
+
+
+
+
+        }
+        public void ()
+        {
+            List<long> idList = new List<long>();
+            
+            
+
+        }
+    }
 
 
 
@@ -113,4 +146,4 @@ namespace Labs216.ArslanovTI
 
 
     }
-}
+
