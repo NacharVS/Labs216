2a51bf10fd7cb56f3c3972445510742e76800997
Dimukhametov <ilnazdima@mail.ru>
2020-12-11 13:18:40 +0300

    DO

---
+++ Labs216/Dimukhametov/trash/StudyBank.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Dimukhametov.trash
+{
+    class StudyBank
+    {
+        
+        public string()
+    }
+    // Есть имя машины  и стоимость машины
+    // Два метода изменить имя машины и изменить стоимость машины
+}   // у денег и имени есть свойства get, set
+    // в свойстве set вызывается ивент который по сути делегат
+    //
