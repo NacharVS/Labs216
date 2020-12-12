a8dfe73e73f7654e9ef0f1ab64c5e43789e727d2
bu1at <gabdulkhaev03@mail.ru>
2020-12-12 12:56:12 +0300

    para

--- Labs216/Gabdulkhaev/Bank.cs
+++ Labs216/Gabdulkhaev/Bank.cs
@@ -44,8 +44,26 @@ namespace Labs216.Gabdulkhaev
         }
         public void SetID(string newID)
         {
-            Random rnd = new Random();
-            int _id = rnd.Next(100, 250);
+            Random rnd3 = new Random();
+            int _id1 = rnd3.Next(100, 250);
+            Random rnd2 = new Random();
+            int _id2 = rnd2.Next(250, 500);
+            Random rnd1 = new Random();
+            int _id3 = rnd1.Next(500, 750);
+            Random rnd0 = new Random();
+            int _id4 = rnd0.Next(750, 1000);
+            // сначала ген 4 числа, потом делим на 4 числа
+            Random rnd5 = new Random();
+            int _id5 = rnd5.Next(2,20);
+            Random rnd6 = new Random();
+            int _id6 = rnd6.Next(20, 40);
+            Random rnd7 = new Random();
+            int _id7 = rnd7.Next(40, 60);
+            Random rnd8 = new Random();
+            int _id8 = rnd8.Next(60, 80);
+            Random rnd9 = new Random();
+            int _idconech = rnd9.Next(1, 10);
+            int id = (_id1 / _id5 + _id2 / _id6 + _id3 / _id7 + _id4 / _id8) * _idconech;
         }
         public double chet;
         double _paymentaccount = Convert.ToDouble(Console.ReadLine());
@@ -144,7 +162,7 @@ namespace Labs216.Gabdulkhaev
     }
     class Progr
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // через ctrl + F искал, больше точек входа нет
         {
             List<Bank> Banklist = new List<Bank>();
             Banklist.Add(new Bank("Имя1", "Фамилия1", 20, 1, new DateTime(2000,1,1)));
@@ -164,7 +182,7 @@ namespace Labs216.Gabdulkhaev
             ChangeSurname(Banklist, 5, "Фамилия20");
             foreach (var item in Banklist)
             {
-                Console.WriteLine(item.Name, item.Age);
+                Console.WriteLine($"{item.Name}");
             }
         }
         static void ChangeName(List<Bank> List, int naitiId, string NewName)
@@ -197,10 +215,11 @@ namespace Labs216.Gabdulkhaev
             {
                 if (item.Id == naitiId)
                     item.Age = NewAge;
-            }
+             }
         }
-
-        // имя фамилия телефон айди возраст дата ржд
-        // в программ лист назв. листа.адд и слово1
+      //  static void ChangeAge1(Dictionary<int,Bank>bankList, int searchId, string newAge)
+       // {
+  //          bankList[searchId].SetAge(newAge);
+    //    }
     }
 }
---
+++ Labs216/Gabdulkhaev/DicWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Gabdulkhaev
+{
+    class DicWork
+    {
+       // Dictionary<int, Bank> BankList = new Dictionary<int, Bank>();
+      //  Bank.add()
+    }
+}
