52d7bbe34135a9fb90ac93d04bbfa8f51218fab4
shay407 <world.deep@mail.ru>
2020-12-12 12:55:28 +0300

    papka

---
+++ Labs216/Shakirov/Bank/Account.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Shakirov
+{
+    class Account
+    {
+        public delegate void OnAccount(string message);
+        public event OnAccount Check = (string message) => Console.WriteLine(message);
+        public Account(string name, string surname, int year, int month)
+        {
+            Name = name;
+            surname = surname;
+            year = year;
+            month = month;
+        }
+
+        private string _name;
+        private string _surname;
+        private string _id;
+        private static int _count;
+        private double _account;
+        private int Year { get; set; }
+        private int Month { get; set; }
+
+        public string Name
+        {
+            get => _name;
+            set => _name = $"{value[0].ToString().ToUpper() + value.Substring(1)}";
+        }
+
+        public string Surname
+        {
+            get => _surname;
+            set => _surname = $"{value[0].ToString().ToUpper() + value.Substring(1)}";
+        }
+        public double Sum
+        {
+            get { return _account; }
+            set
+            {
+                _account = value;
+                double res = Math.Round(_account, 2);
+                Check($"On Account {res}");
+            }
+
+        }
+    }
+}
