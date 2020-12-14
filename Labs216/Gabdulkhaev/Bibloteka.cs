8b7da2f108d7ec19c1ca2a1ad3f398c133a2f5df
bu1at <gabdulkhaev03@mail.ru>
2020-12-14 12:57:16 +0300

    knigis

---
+++ Labs216/Gabdulkhaev/Bibloteka.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Gabdulkhaev
+{
+    class Bibloteka
+    {
+        public static void Knigi()
+        {
+            Queue<string> knigis = new Queue<string>();
+            knigis.Enqueue("Первая книга");
+            knigis.Enqueue("Вторая книга");
+            knigis.Enqueue("Третья книга");
+            knigis.Enqueue("Четвертая книга");
+            knigis.Enqueue("Пятая книга");
+            string a = knigis.Dequeue();
+            Console.WriteLine(a);
+            foreach (var item in knigis)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
