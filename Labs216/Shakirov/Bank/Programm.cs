210a476c64af7abb917701cb96268de1f73d6d9f
shay407 <world.deep@mail.ru>
2020-12-14 10:22:28 +0300

    StudentList

--- Labs216/Shakirov/Bank/Programm.cs
+++ Labs216/Shakirov/Bank/Programm.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace Labs216.Shakirov
-{
-    class Program
-    {
-        public static void Main(string[] args)
-        {
-            ArrayListTest test = new ArrayListTest();
-            test.Collection();
-        }
-    }
-}
+//namespace Labs216.Shakirov
+//{
+//    class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            ArrayListTest test = new ArrayListTest();
+//            test.Collection();
+//        }
+//    }
+//}
---
+++ Labs216/Shakirov/StudentList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Shakirov
+{
+    class StudentList
+    {
+
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> studentList = new Dictionary<int, string>
+                Random rnd = new Random();
+
+            for(int i = 0; i < 10; i++)
+            {
+                string buff = Console.ReadLine();
+                studentList.Add(rnd.Next(0, 1000), buff);
+            }
+
+            foreach (var item in studentList)
+        }
+    }
+}
