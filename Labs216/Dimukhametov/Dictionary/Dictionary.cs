62289f0aa9f3054d865411f7b4bf289219ed49d1
Dimukhametov <ilnazdima@mail.ru>
2020-12-14 10:50:52 +0300

    GOTOVO

---
+++ Labs216/Dimukhametov/Dictionary/Dictionary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Dimukhametov.Dictionary
+{
+    public class MyBestWork
+    {
+            Dictionary<Guid, string> studentlist = new Dictionary<Guid, string>();
+        public void DictionaryFill()
+        {
+            for (int i = 0; i <= 9; i++)
+            {
+                studentlist.Add(Guid.NewGuid(), Console.ReadLine());
+            }
+            Console.WriteLine();
+
+            foreach (KeyValuePair<Guid, string> people in studentlist)
+            {
+                Console.WriteLine(people.Key + " - " + people.Value);
+            }
+        }
+}   }
---
+++ Labs216/Dimukhametov/Dictionary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Dimukhametov.Dictionary
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyBestWork myBestWork = new MyBestWork();
+            myBestWork.DictionaryFill();
+
+        }
+    }
+}
