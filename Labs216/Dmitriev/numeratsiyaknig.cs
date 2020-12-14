e993b1374bff226e095baf8236604c89aba39906
DSeregaV <ddmmiittrriieevv2003@gmail.com>
2020-12-14 13:21:08 +0300

    p4ti dodelal pro knigi

---
+++ Labs216/Dmitriev/numeratsiyaknig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Dmitriev
+{
+    class numeratsiyaknig
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> numbers = new Queue<int>();
+            numbers.Enqueue(1);
+            numbers.Enqueue(2);
+            numbers.Enqueue(3);
+            int queueElement = numbers.Dequeue();
+            Console.WriteLine(queueElement);
+            Queue<book> books = new Queue<book>();
+            books.Enqueue(new book() { Name = "naruto,Kishimoto Masashi,1999" });
+            books.Enqueue(new book() { Name = "ataka titanov,Xadzime Isayami,2009" });
+            books.Enqueue(new book() { Name = "7 smertnblx grexov,Nakaba Sudzuki 2012" });
+            book pp = books.Peek();
+            Console.WriteLine(pp.Name);
+            Console.WriteLine("сейчас в очереди{0} kniga", books.Count);
+            foreach (book p in books)
+            {
+                Console.WriteLine(p.Name);
+            }
+            book book = books.Dequeue();
+            Console.WriteLine(book.Name);
+            Console.ReadLine();
+        }
+    }
+    class book
+    {
+        public string Name { get; set; }
+    }
+}
+    
