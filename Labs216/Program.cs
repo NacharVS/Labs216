c3bec5bc18569458978f68a308b4e1cf007cabee
Ainur-ctrl <zamaliev.aj@yandex.com>
2020-12-15 12:32:28 +0300

    ochered

--- Labs216/Program.cs
+++ Labs216/Program.cs
@@ -8,41 +8,9 @@ namespace Labs216
     {
         static void Main(string[] args)
         {
-            Bank Ivana = new Bank(0, 0.5, "88005553535");
-            Ivana.Notify += Message;
-            while (true)
-            {
-                switch (Console.ReadLine())
-                {
-                    case "1":
-                        Ivana.Deposit(int.Parse(Console.ReadLine()));
-                        break;
-                    case "2":
-                        Ivana.Snyat(int.Parse(Console.ReadLine()));
-                        break;
-                    default:
-                        Console.WriteLine("vi razvalili sssr");
-                        break;
-                }
-                Console.WriteLine($"Баланс: {Ivana.Balans}");
-            }          
-        }
-        public static void Message(int sum, string phonenumber)
-        {
-            Console.WriteLine($"Message send at {phonenumber}. {sum}");
-        }
-        public static void Message2(int sum, string phonenumber)
-        {
-            Console.WriteLine($"Account value {sum}");
-        }
-        public static void Message3(double stavka)
-        {
-            Console.WriteLine($"Our stavka {stavka}");
-        }
-        public static void Message4(int sum, double cashback)
-        {
-            Console.WriteLine($"Return of a part of funds: {cashback}");
+            QueueAndStack.Reading();
         }
     }
 }
+        
 
---
+++ Labs216/Zamaliev/QueueAndStack.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Zamaliev
+{
+    class QueueAndStack
+    {
+       public static void Reading()
+        {
+            Queue<string> Books = new Queue<string>();
+
+            Books.Enqueue("Михаил Булгаков — Мастер и Маргарита,1940");
+            Books.Enqueue("Николай Гоголь — Мёртвые души,1842");
+            Books.Enqueue("Михаил Булгаков — Собачье сердце,1925");
+            Books.Enqueue("Федор Достоевский — Преступление и наказание,1866");
+            Books.Enqueue("Лев Толстой — Война и мир,1868");
+            
+            foreach(string item in Books)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
