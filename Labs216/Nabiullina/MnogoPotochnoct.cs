01d7b2c9304b02282f2bfc543bf2b78aab3a65e6
Zulfia <zulfianabiullina4998@.com>
2020-12-17 14:57:36 +0300

    mnogoPotochnost'

---
+++ Labs216/Nabiullina/MnogoPotochnoct.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labs216.Nabiullina
+{
+    class MnogoPotochnoct
+    {
+        Thread timur = new Thread(new ParameterizedThreadStart());
+        public static void thread1()
+        {
+            
+        }
+
+    }
+}
