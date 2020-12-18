78ea8cdf16b0707df29cb8304ad46ee0365a5ea7
bu1at <gabdulkhaev03@mail.ru>
2020-12-18 13:16:40 +0300

    ne

---
+++ Labs216/Gabdulkhaev/Massives.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labs216.Gabdulkhaev
+{
+    class Massives
+    {
+        //public static void op()
+        //{
+        //    Task task1=new Task()=>Massiffi;
+        //    task1.Start();
+        //    task1.Wait();
+        //    Console.WriteLine("res");
+        //}
+        /* два одномерных массивов
+         * 1)заполнить
+         * 2) сложить
+         * 3) отсортировать то что сложили
+         * при сложении этих двух массивов заполнить первые две ячейки и сразу же их сложить
+         * запустить процесс сложения чуть позже, чем процесс заполнения
+         * Сортировку запустить только после получения ответа
+         */
+            
+        
+    }
+}
