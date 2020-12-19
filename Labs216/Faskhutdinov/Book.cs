be3bf100f75913fd67b3681d4513dcae709df1a7
islam7898 <is.fs.ilsh@gmail.com>
2020-12-19 12:54:35 +0300

    add book and tasks

---
+++ Labs216/Faskhutdinov/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Faskhutdinov
+{
+    class Book
+    {
+        public Book()
+        {
+        }
+
+    }
+}
