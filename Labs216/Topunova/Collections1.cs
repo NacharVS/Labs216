f450f8b24d320351c0d7550d898773fd95fbb4b1
Kate.Top216 <k_top683@mail.ru>
2020-12-20 19:44:54 +0300

    Начало

---
+++ Labs216/Topunova/Collections1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Topunova
+{
+    class Collections1
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 new int[5];
+            int[] arr2 new int[5];
+            int[] arr3 new int[5];
+
+            Task taskOne = new Task(() =>
+            {
+                for (int i = 0; 1 < arr1.Length; i++)
+                {
+                    arr1[1] = new Random().Next(10);
+                    arr2[1] = new Random().Next(10);
+
+                    Console.WriteLine($"arr1[{i}] = {arr1[i]} arr2[{i}] = {arr2[i]}");
+                }
+            }
+        }
+    }
+}
