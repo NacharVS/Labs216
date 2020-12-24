1779775d04ab9f1d726c722ef6225c43b4d8369a
Azaliya <amalkaazaliya@gmail.com>
2020-12-24 09:31:36 +0300

    All right!

---
+++ Labs216/December/MultiThreading.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Labs216.Multithreading
+{
+    class MultiThreading
+    {
+        public static void Array()
+        {
+            int[] array1 = new int[10];
+            int[] array2 = new int[10];
+            int[] sumarray = new int[10];
+            Random rnd = new Random();
+
+            Task task1 = new Task(() =>
+            {
+                for (int i = 0; i < array1.Length; i++)
+                {
+                    array1[i] = rnd.Next(10, 100);
+                    array2[i] = rnd.Next(10, 100);
+                    sumarray[i] = array1[i] + array2[i];
+                    Console.WriteLine($"{i})  {array1[i]}    {i})  {array2[i]} = {sumarray[i]} ");
+
+                }
+            });
+            task1.Start();
+            task1.Wait();
+
+           
+        }
+    }
+}
--- Labs216/Program.cs
+++ Labs216/Program.cs
@@ -12,6 +12,7 @@ using Labs216.December;
 using System.Threading;
 using System.Threading.Tasks;
 using System;
+using Labs216.Multithreading;
 
 namespace Labs216
 {
@@ -19,35 +20,7 @@ namespace Labs216
     {
         static void Main(string[] args)
         {
-            int[] mass1 = new int[10];
-            Random rnd = new Random();
-            int[] mass2 = new int[10];
-            int Sum = 0;
-
-            Console.WriteLine("First mass:");
-            for (int i = 0; i < mass1.Length; i++)
-            {
-                mass1[i] = rnd.Next(10, 99);
-                Console.Write($" {mass1[i]}");
-            }
-            Console.WriteLine();
-
-            Console.WriteLine("Second mass:");
-            for (int i = 0; i < mass2.Length; i++)
-            {
-                mass2[i] = rnd.Next(10, 99);
-                Console.Write($" {mass2[i]}");
-            }
-            Console.WriteLine();
-
-            Console.Write("SumTwoMass:");
-            for (int s = 0; s < mass1.Length; s++)
-            {
-                if (mass1[s] > 0)
-                    Sum = mass1[s] + mass2[s];
-                Console.Write($" {Sum}");
-            }
-            Console.WriteLine();
+            MultiThreading.Array();
         }
 
     }
