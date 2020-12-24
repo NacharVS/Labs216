ae816069ad026a3ef2a5baf7ea318961f7b158c6
islam7898 <is.fs.ilsh@gmail.com>
2020-12-24 09:35:55 +0300

    book

--- Labs216/Faskhutdinov/Book.cs
+++ Labs216/Faskhutdinov/Book.cs
@@ -1,14 +1,40 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Labs216.Faskhutdinov
 {
-    class Book
+    class Books
     {
-        public Book()
+        public static void BookOfRead()
         {
-        }
+            Stack<string> myBook = new Stack<string>();
+            myBook.Push("Tolstiy i tonciy(Tolstoy)");
+            myBook.Push("Voina i mir(Tolstoy)");
+            myBook.Push("Lev i sobaca(Tolstoy)");
+            myBook.Push("U mory(Stivenking)");
+            myBook.Push("yazikc#(Faskhutdinov)");
 
+            string a = myBook.Pop();
+            string b = myBook.Pop();
+            string c = myBook.Pop();
+            string d = myBook.Pop();
+            string e = myBook.Pop();
+            Console.WriteLine(a, b);
+
+
+
+
+
+        }
     }
 }
+
+
+
+
+
+
+
+
