519481c26a4fb72a9b7786e69c28aff97426aa68
bu1at <gabdulkhaev03@mail.ru>
2021-01-19 16:47:56 +0300

    -

---
+++ Labs216/Gabdulkhaev/BankBD.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs216.Gabdulkhaev
+{
+    class BankBD
+    {
+       // все, что работает с базами данных
+    }
+}
