e7d265349feb3d72868fe22122ca71e9368694af
Khabibullina216 <161918@mck-ktits.ru>
2021-01-19 15:55:41 +0300

    ok (ne bank)

---
+++ Labs216/Khabibullina/badedann1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+
+namespace Labs216.Khabibullina
+{
+    class Student
+    {
+        [BsonId]
+        public int _id;
+        [BsonElement("Name")]
+        public string name;
+        [BsonIgnoreIfDefault]
+        public int age;
+
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Student student1 = new Student { _id = 666, name = "Satan", age = 999999 };
+            Student student2 = new Student { _id = 5478, name = "Elizabeth Bathory The Best Woman in the World", age = 46 };
+            MongoIncert(student1).GetAwaiter().GetResult();
+            MongoIncert(student2).GetAwaiter().GetResult();
+        }
+
+        static async Task MongoIncert(Student student)
+        {
+            string connectionString = "mongodb://localhost";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("216TeamDB");
+            var collection = database.GetCollection<Student>("Students");
+            await collection.InsertManyAsync(student);
+        }
+        static async Task MongoIncert()
+        {
+            string connectionString = "mongodb://localhost";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("216TeamDB");
+            var collection = database.GetCollection<Student>("Students");
+            var student = new BsonDocument();
+            var students = await collection.Find(student).ToListAsync();
+
+            foreach (var item in students)
+            {
+                Console.WriteLine(item.name);
+                Console.WriteLine(item._id);
+                Console.WriteLine(item.age);
+            }
+        }
+    }
+}
+
