d1e34c3bee50ba08abcf28d087d6dce32aae918f
bu1at <gabdulkhaev03@mail.ru>
2021-01-21 15:08:50 +0300

    mongo

--- Labs216/Gabdulkhaev/BankBD.cs
+++ Labs216/Gabdulkhaev/BankBD.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +8,24 @@ namespace Labs216.Gabdulkhaev
 {
     class BankBD
     {
+        [BsonId]
+        public ObjectId _id;
+        [BsonElement("Imya")]
+        public string name;
+        [BsonElement("Familiya")]
+        public string surname;
+        [BsonIgnoreIfDefault]
+        public int age;
+    }
+    static void PoiskPoImeni(string searchName)
+    {
+        string connectionString = "mongodb://localhost";
+        var client = new MongoClient(connectionString);
+        var database=client.GetDatabase()
+    }
+    {
+
+    }
        // все, что работает с базами данных
     }
 }
