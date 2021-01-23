39597246c7254f4401cd14c73327b2673f266707
bu1at <gabdulkhaev03@mail.ru>
2021-01-23 12:58:15 +0300

    регистратион

--- Labs216/Gabdulkhaev/BankBD.cs
+++ Labs216/Gabdulkhaev/BankBD.cs
@@ -4,6 +4,7 @@ using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Labs216.Gabdulkhaev
 {
@@ -18,5 +19,22 @@ namespace Labs216.Gabdulkhaev
         [BsonIgnoreIfDefault]
         public int age;
     }
-       // все, что работает с базами данных
+    // все, что работает с базами данных
+    class Student
+    {
+        static void Main()
+        {
+            Student student1 = new Student { name = "Igor", age = 15 };
+            Student student2 = new Student { name = "Maksim" };
+            MongoInsert(student1).GetAwaiter().GetResult();
+        }
+        static async Task MongoInsert(Student student)
+        {
+            string connectionString = "mongodb://localhost";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("216TeamDB");
+            var collection = database.GetCollection<Student>("Students");
+            await collection.InsertOneAsync(student);
+        }
+}
     }
