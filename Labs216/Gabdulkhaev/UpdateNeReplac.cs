3b485686f981bfaca32507ee36db755c86f314ff
bu1at <gabdulkhaev03@mail.ru>
2021-02-02 16:42:58 +0300

    сделали аптаде

---
+++ Labs216/Gabdulkhaev/UpdateNeReplac.cs
@@ -0,0 +1,80 @@
+﻿using Labs216.Gabdulkhaev;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labs216.Gabdulkhaev
+{
+    class UpdateNeReplac
+    {
+            [BsonId]
+            [BsonIgnoreIfDefault]
+            public ObjectId _id;
+            [BsonElement("Imya")]
+            public string name;
+            [BsonElement("Familiya")]
+            public string surname;
+            [BsonIgnoreIfDefault]
+            public int age;
+            [BsonIgnoreIfDefault]
+            public DateTime dateTime;
+            [BsonIgnoreIfNull]
+            public string Email;
+        }
+    }
+
+class Prog
+    {
+        static async Task MongoUpdate(string searchName, int newAge)
+        {
+            string connectionString = "mongodb://localhost";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("CosmicCaramel");
+            var collection = database.GetCollection<Student>("PS");
+            var update = Builders<Student>.Update.Set(a => a.age, newAge);
+            await collection.UpdateOneAsync(x => x.name == searchName, update);
+        }
+        static async Task SearchByName(string searchName, string searchSurname)
+        {
+            string connectionString = "mongodb://localhost";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("216TeamDB");
+            var collection = database.GetCollection<Student>("Students");
+            var students = await collection.Find(std => std.name == searchName & std.surname == searchSurname).ToListAsync();
+            foreach (var item in students)
+            {
+                Console.WriteLine(item.name);
+            }
+        }
+        static async Task MongoInsert(Student student)
+        {
+            string connectionString = "mongodb://localhost";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("216TeamDB");
+            var collection = database.GetCollection<Student>("Students");
+            await collection.InsertOneAsync(student);
+        }
+        static async Task MongoConnect()
+        {
+            string connectionString = "mongodb://localhost";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("216TeamDB");
+            var collection = database.GetCollection<Student>("Students");
+            var student = new BsonDocument();
+            var students = await collection.Find(student).ToListAsync();
+            foreach (var item in students)
+            {
+                Console.WriteLine(item.name);
+                Console.WriteLine(item._id);
+                Console.WriteLine(item.age);
+                static void Main()
+                {
+                    MongoConnect().GetAwaiter().GetResult();
+                }
+            }
+        }
+    }
