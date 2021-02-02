17669844f4ca92af5512322024243286707915ed
islam7898 <is.fs.ilsh@gmail.com>
2021-02-02 16:30:15 +0300

    reg

--- Labs216/Faskhutdinov/Replace.cs
+++ Labs216/Faskhutdinov/Replace.cs
@@ -1,40 +1,47 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Labs216.Faskhutdinov
 {
-    [BsonId]
-    [BsonIgnoreIfDefault]
-    public ObjectId id;
-    [BsonElement("Islam")]
-    public string name;
-    [BsonElement("Ilnaz")]
-    public string sername;
-    [BsonIgnoreIfDefault]
-    public int age;
-    [BsonIgnoreIfDefault]
-    public DataTime dataTime;
-    [BsonIgnoreIfNull]
-    public string Email;
+    class Student
+    {
+        [BsonId]
+        [BsonIgnoreIfDefault]
+        public ObjectId id;
+        [BsonElement("Islam")]
+        public string name;
+        [BsonElement("Ilnaz")]
+        public string sername;
+        [BsonIgnoreIfDefault]
+        public int age;
+        [BsonIgnoreIfNull]
+        public string Email;
+    }
     class Replace
     {
-        public static void Main(string[] args)
+        private static async Task MongoReplaceByName (string searchName, int newAge)
         {
-            string connection = "mongodb://localhost";
-            var client = new MongoClient(connection);
+            string connectionString = "mongodb://localhost";
+            var client = new MongoClient(connectionString);
             var database = client.GetDatabase("MAret");
             var collection = database.GetCollection<People>("Students");
-            var std = collection.Find(std => std.Surname == "Faskhutdinov").ToListAsync().GetAwaiter().GetResult();
-            People imf = new People("Islam", "Faskhutdinov", 17, 3940293);
-            collection.InsertOneAsync(imf).GetAwaiter().GetResult();
+            var update = Builders<Student>.Update.Set(a => a.age, newAge);
+            await collection.UpdateOneAsync((a => a.name == searchName), update);
 
-            foreach (var item in std)
-            {
-                Console.WriteLine(item.Name);
-            }
+        }
+        static async Task MongoreplaceByName(string searchName, Student newStudent)
+        {
+            string connectionString = "mongodb://localhost";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("MAret");
+            var collection = database.GetCollection<People>("Students");
+            var update = Builders<Student>.Update.Set(a => , newStudent);
+            await collection.UpdateOneAsync(std => std.name == searchName, update);
         }
     }
 }
