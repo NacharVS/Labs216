983a0f10463a289e0f4490f0be818ee8ec9ceb3b
Kate.Top216 <k_top683@mail.ru>
2021-02-14 18:21:15 +0300

    kalkulator

--- Labs216/Topunova/Strategy/Abstract.cs
+++ Labs216/Topunova/Strategy/Abstract.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace Labs216.Topunova.Strategy
-{
-    abstract class Characteristics
-    {
-        public int Health { get; set; }
-        public int Speed { get; set;}
-    }
-}
+//namespace Labs216.Topunova.Strategy
+//{
+//    abstract class Characteristics
+//    {
+//        public int Health { get; set; }
+//        public int Speed { get; set;}
+//    }
+//}
