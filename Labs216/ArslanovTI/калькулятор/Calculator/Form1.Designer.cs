8185ff1e11c492033714aa8062a3ece190f4b8ec
FZQXLOL <timurrikkert@yandex.ru>
2021-02-27 01:34:17 +0300

    =хжздльот

---
+++ Labs216/ArslanovTI/калькулятор/Calculator/Form1.Designer.cs
@@ -0,0 +1,395 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.one = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.rovno = new System.Windows.Forms.Button();
+            this.two = new System.Windows.Forms.Button();
+            this.three = new System.Windows.Forms.Button();
+            this.four = new System.Windows.Forms.Button();
+            this.five = new System.Windows.Forms.Button();
+            this.six = new System.Windows.Forms.Button();
+            this.seven = new System.Windows.Forms.Button();
+            this.eight = new System.Windows.Forms.Button();
+            this.nine = new System.Windows.Forms.Button();
+            this.zero = new System.Windows.Forms.Button();
+            this.div = new System.Windows.Forms.Button();
+            this.mul = new System.Windows.Forms.Button();
+            this.defect = new System.Windows.Forms.Button();
+            this.point = new System.Windows.Forms.Button();
+            this.one_div_x = new System.Windows.Forms.Button();
+            this.sqrt = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.log = new System.Windows.Forms.Button();
+            this.cos = new System.Windows.Forms.Button();
+            this.sin = new System.Windows.Forms.Button();
+            this.tan = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // one
+            // 
+            this.one.Location = new System.Drawing.Point(12, 204);
+            this.one.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.one.Name = "one";
+            this.one.Size = new System.Drawing.Size(35, 35);
+            this.one.TabIndex = 1;
+            this.one.Text = "1";
+            this.one.UseVisualStyleBackColor = true;
+            this.one.Click += new System.EventHandler(this.one_Click);
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(138, 240);
+            this.plus.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(35, 35);
+            this.plus.TabIndex = 2;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // rovno
+            // 
+            this.rovno.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rovno.Location = new System.Drawing.Point(179, 204);
+            this.rovno.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.rovno.Name = "rovno";
+            this.rovno.Size = new System.Drawing.Size(35, 71);
+            this.rovno.TabIndex = 3;
+            this.rovno.Text = "=";
+            this.rovno.UseVisualStyleBackColor = true;
+            this.rovno.Click += new System.EventHandler(this.rovno_Click);
+            // 
+            // two
+            // 
+            this.two.Location = new System.Drawing.Point(48, 204);
+            this.two.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.two.Name = "two";
+            this.two.Size = new System.Drawing.Size(35, 35);
+            this.two.TabIndex = 4;
+            this.two.Text = "2";
+            this.two.UseVisualStyleBackColor = true;
+            this.two.Click += new System.EventHandler(this.two_Click);
+            // 
+            // three
+            // 
+            this.three.Location = new System.Drawing.Point(84, 204);
+            this.three.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.three.Name = "three";
+            this.three.Size = new System.Drawing.Size(35, 35);
+            this.three.TabIndex = 5;
+            this.three.Text = "3";
+            this.three.UseVisualStyleBackColor = true;
+            this.three.Click += new System.EventHandler(this.three_Click);
+            // 
+            // four
+            // 
+            this.four.Location = new System.Drawing.Point(12, 168);
+            this.four.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.four.Name = "four";
+            this.four.Size = new System.Drawing.Size(35, 35);
+            this.four.TabIndex = 6;
+            this.four.Text = "4";
+            this.four.UseVisualStyleBackColor = true;
+            this.four.Click += new System.EventHandler(this.four_Click);
+            // 
+            // five
+            // 
+            this.five.Location = new System.Drawing.Point(48, 168);
+            this.five.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.five.Name = "five";
+            this.five.Size = new System.Drawing.Size(35, 35);
+            this.five.TabIndex = 7;
+            this.five.Text = "5";
+            this.five.UseVisualStyleBackColor = true;
+            this.five.Click += new System.EventHandler(this.five_Click);
+            // 
+            // six
+            // 
+            this.six.Location = new System.Drawing.Point(84, 168);
+            this.six.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.six.Name = "six";
+            this.six.Size = new System.Drawing.Size(35, 35);
+            this.six.TabIndex = 8;
+            this.six.Text = "6";
+            this.six.UseVisualStyleBackColor = true;
+            this.six.Click += new System.EventHandler(this.six_Click);
+            // 
+            // seven
+            // 
+            this.seven.Location = new System.Drawing.Point(12, 131);
+            this.seven.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.seven.Name = "seven";
+            this.seven.Size = new System.Drawing.Size(35, 35);
+            this.seven.TabIndex = 9;
+            this.seven.Text = "7";
+            this.seven.UseVisualStyleBackColor = true;
+            this.seven.Click += new System.EventHandler(this.seven_Click);
+            // 
+            // eight
+            // 
+            this.eight.Location = new System.Drawing.Point(48, 131);
+            this.eight.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.eight.Name = "eight";
+            this.eight.Size = new System.Drawing.Size(35, 35);
+            this.eight.TabIndex = 10;
+            this.eight.Text = "8";
+            this.eight.UseVisualStyleBackColor = true;
+            this.eight.Click += new System.EventHandler(this.eight_Click);
+            // 
+            // nine
+            // 
+            this.nine.Location = new System.Drawing.Point(84, 131);
+            this.nine.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.nine.Name = "nine";
+            this.nine.Size = new System.Drawing.Size(35, 35);
+            this.nine.TabIndex = 11;
+            this.nine.Text = "9";
+            this.nine.UseVisualStyleBackColor = true;
+            this.nine.Click += new System.EventHandler(this.nine_Click);
+            // 
+            // zero
+            // 
+            this.zero.Location = new System.Drawing.Point(12, 240);
+            this.zero.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.zero.Name = "zero";
+            this.zero.Size = new System.Drawing.Size(71, 35);
+            this.zero.TabIndex = 12;
+            this.zero.Text = "0";
+            this.zero.UseVisualStyleBackColor = true;
+            this.zero.Click += new System.EventHandler(this.zero_Click);
+            // 
+            // div
+            // 
+            this.div.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.div.Location = new System.Drawing.Point(138, 131);
+            this.div.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.div.Name = "div";
+            this.div.Size = new System.Drawing.Size(35, 35);
+            this.div.TabIndex = 13;
+            this.div.Text = "/";
+            this.div.UseVisualStyleBackColor = true;
+            this.div.Click += new System.EventHandler(this.div_Click);
+            // 
+            // mul
+            // 
+            this.mul.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.mul.Location = new System.Drawing.Point(138, 168);
+            this.mul.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.mul.Name = "mul";
+            this.mul.Size = new System.Drawing.Size(35, 35);
+            this.mul.TabIndex = 14;
+            this.mul.Text = "*";
+            this.mul.UseVisualStyleBackColor = true;
+            this.mul.Click += new System.EventHandler(this.mul_Click);
+            // 
+            // defect
+            // 
+            this.defect.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.defect.Location = new System.Drawing.Point(138, 204);
+            this.defect.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.defect.Name = "defect";
+            this.defect.Size = new System.Drawing.Size(35, 35);
+            this.defect.TabIndex = 15;
+            this.defect.Text = "-";
+            this.defect.UseVisualStyleBackColor = true;
+            this.defect.Click += new System.EventHandler(this.defect_Click);
+            // 
+            // point
+            // 
+            this.point.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.point.Location = new System.Drawing.Point(84, 240);
+            this.point.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.point.Name = "point";
+            this.point.Size = new System.Drawing.Size(35, 35);
+            this.point.TabIndex = 16;
+            this.point.Text = ",";
+            this.point.UseVisualStyleBackColor = true;
+            this.point.Click += new System.EventHandler(this.point_Click);
+            // 
+            // one_div_x
+            // 
+            this.one_div_x.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.one_div_x.Location = new System.Drawing.Point(179, 168);
+            this.one_div_x.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.one_div_x.Name = "one_div_x";
+            this.one_div_x.Size = new System.Drawing.Size(35, 35);
+            this.one_div_x.TabIndex = 17;
+            this.one_div_x.Text = "1/x";
+            this.one_div_x.UseVisualStyleBackColor = true;
+            this.one_div_x.Click += new System.EventHandler(this.one_div_x_Click);
+            // 
+            // sqrt
+            // 
+            this.sqrt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sqrt.Location = new System.Drawing.Point(179, 131);
+            this.sqrt.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.sqrt.Name = "sqrt";
+            this.sqrt.Size = new System.Drawing.Size(35, 35);
+            this.sqrt.TabIndex = 18;
+            this.sqrt.Text = "√";
+            this.sqrt.UseVisualStyleBackColor = true;
+            this.sqrt.Click += new System.EventHandler(this.sqrt_Click);
+            // 
+            // delete
+            // 
+            this.delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.delete.Location = new System.Drawing.Point(12, 51);
+            this.delete.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(107, 35);
+            this.delete.TabIndex = 19;
+            this.delete.Text = "Очистить";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // log
+            // 
+            this.log.Location = new System.Drawing.Point(318, 91);
+            this.log.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.log.Name = "log";
+            this.log.Size = new System.Drawing.Size(35, 35);
+            this.log.TabIndex = 31;
+            this.log.Text = "log";
+            this.log.UseVisualStyleBackColor = true;
+            this.log.Click += new System.EventHandler(this.log_Click);
+            // 
+            // cos
+            // 
+            this.cos.Location = new System.Drawing.Point(241, 131);
+            this.cos.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.cos.Name = "cos";
+            this.cos.Size = new System.Drawing.Size(35, 35);
+            this.cos.TabIndex = 36;
+            this.cos.Text = "cos";
+            this.cos.UseVisualStyleBackColor = true;
+            this.cos.Click += new System.EventHandler(this.cos_Click);
+            // 
+            // sin
+            // 
+            this.sin.Location = new System.Drawing.Point(241, 168);
+            this.sin.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.sin.Name = "sin";
+            this.sin.Size = new System.Drawing.Size(35, 35);
+            this.sin.TabIndex = 37;
+            this.sin.Text = "sin";
+            this.sin.UseVisualStyleBackColor = true;
+            this.sin.Click += new System.EventHandler(this.sin_Click);
+            // 
+            // tan
+            // 
+            this.tan.Location = new System.Drawing.Point(242, 204);
+            this.tan.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.tan.Name = "tan";
+            this.tan.Size = new System.Drawing.Size(35, 35);
+            this.tan.TabIndex = 38;
+            this.tan.Text = "tan";
+            this.tan.UseVisualStyleBackColor = true;
+            this.tan.Click += new System.EventHandler(this.tan_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 12);
+            this.textBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(202, 20);
+            this.textBox.TabIndex = 0;
+            this.textBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 287);
+            this.Controls.Add(this.tan);
+            this.Controls.Add(this.sin);
+            this.Controls.Add(this.cos);
+            this.Controls.Add(this.log);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.sqrt);
+            this.Controls.Add(this.one_div_x);
+            this.Controls.Add(this.point);
+            this.Controls.Add(this.defect);
+            this.Controls.Add(this.mul);
+            this.Controls.Add(this.div);
+            this.Controls.Add(this.zero);
+            this.Controls.Add(this.nine);
+            this.Controls.Add(this.eight);
+            this.Controls.Add(this.seven);
+            this.Controls.Add(this.six);
+            this.Controls.Add(this.five);
+            this.Controls.Add(this.four);
+            this.Controls.Add(this.three);
+            this.Controls.Add(this.two);
+            this.Controls.Add(this.rovno);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.one);
+            this.Controls.Add(this.textBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "считать денюжки";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button one;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button rovno;
+        private System.Windows.Forms.Button two;
+        private System.Windows.Forms.Button three;
+        private System.Windows.Forms.Button four;
+        private System.Windows.Forms.Button five;
+        private System.Windows.Forms.Button six;
+        private System.Windows.Forms.Button seven;
+        private System.Windows.Forms.Button eight;
+        private System.Windows.Forms.Button nine;
+        private System.Windows.Forms.Button zero;
+        private System.Windows.Forms.Button div;
+        private System.Windows.Forms.Button mul;
+        private System.Windows.Forms.Button defect;
+        private System.Windows.Forms.Button point;
+        private System.Windows.Forms.Button one_div_x;
+        private System.Windows.Forms.Button sqrt;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button log;
+        private System.Windows.Forms.Button cos;
+        private System.Windows.Forms.Button sin;
+        private System.Windows.Forms.Button tan;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
+
